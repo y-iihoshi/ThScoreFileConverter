acf467444f6d1e18b85eeb0e85f9562ff63fd775
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2017-11-11 20:16:25 +0900

    Add the unit tests for Models.Utils

---
+++ ThScoreFileConverterTests/Models/UtilsTests.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+using System.Text;
+
+namespace ThScoreFileConverter.Models.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        private bool? outputSeparator = null;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod()]
+        public void ParseEnumTest_ValidName()
+        {
+            Assert.AreEqual(DayOfWeek.Sunday, Utils.ParseEnum<DayOfWeek>("Sunday"));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseEnumTest_InvalidName()
+        {
+            var value = Utils.ParseEnum<DayOfWeek>("Sun");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseEnumTest_Empty()
+        {
+            var value = Utils.ParseEnum<DayOfWeek>(string.Empty);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ParseEnumTest_Null()
+        {
+            var value = Utils.ParseEnum<DayOfWeek>(null);
+        }
+
+        [TestMethod()]
+        public void ParseEnumTest_CaseSensitive_ValidName()
+        {
+            Assert.AreEqual(DayOfWeek.Sunday, Utils.ParseEnum<DayOfWeek>("Sunday", false));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseEnumTest_CaseSensitive_InvalidName()
+        {
+            var value = Utils.ParseEnum<DayOfWeek>("sunday", false);
+        }
+
+        [TestMethod()]
+        public void ParseEnumTest_CaseInsensitive_ValidName()
+        {
+            Assert.AreEqual(DayOfWeek.Sunday, Utils.ParseEnum<DayOfWeek>("Sunday", true));
+            Assert.AreEqual(DayOfWeek.Sunday, Utils.ParseEnum<DayOfWeek>("sunday", true));
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            // FIXME: The method to be tested should be renamed.
+            var enumerable = Utils.GetEnumerator<DayOfWeek>();
+            var i = (int)DayOfWeek.Sunday;
+            foreach (var value in enumerable)
+                Assert.AreEqual((DayOfWeek)i++, value);
+            Assert.AreEqual(7, i);
+        }
+
+        [TestMethod()]
+        public void ToNumberStringTest()
+        {
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("12,345,678", Utils.ToNumberString(12345678));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("12345678", Utils.ToNumberString(12345678));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ToNumberStringTest_Unset()
+        {
+            Settings.Instance.OutputNumberGroupSeparator = null;
+            var value = Utils.ToNumberString(1234);
+        }
+
+        [TestMethod()]
+        public void ToNumberStringTest_WithSeparator()
+        {
+            Assert.AreEqual("12,345,678", Utils.ToNumberString(12345678, true));
+        }
+
+        [TestMethod()]
+        public void ToNumberStringTest_WithoutSeparator()
+        {
+            Assert.AreEqual("12345678", Utils.ToNumberString(12345678, false));
+        }
+
+        [TestMethod()]
+        public void FormatTest()
+        {
+            var now = DateTime.Now;
+            Assert.AreEqual(
+                string.Format(CultureInfo.CurrentCulture, "{0:F}", now),
+                Utils.Format("{0:F}", now));
+        }
+
+        [TestMethod()]
+        public void MakeAndPredicateTest()
+        {
+            var pred = Utils.MakeAndPredicate<int>(x => (x > 3), x => (x < 5));
+            Assert.IsFalse(pred(3));
+            Assert.IsTrue(pred(4));
+            Assert.IsFalse(pred(5));
+        }
+
+        [TestMethod()]
+        public void ToZeroBasedTest()
+        {
+            Assert.AreEqual(0, Utils.ToZeroBased(1));
+            Assert.AreEqual(1, Utils.ToZeroBased(2));
+            Assert.AreEqual(8, Utils.ToZeroBased(9));
+            Assert.AreEqual(9, Utils.ToZeroBased(0));   // Hmm...
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToZeroBasedTest_Negative()
+        {
+            var value = Utils.ToZeroBased(-1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToZeroBasedTest_Exceeded()
+        {
+            var value = Utils.ToZeroBased(10);
+        }
+
+        [TestMethod()]
+        public void ToOneBasedTest()
+        {
+            Assert.AreEqual(1, Utils.ToOneBased(0));
+            Assert.AreEqual(2, Utils.ToOneBased(1));
+            Assert.AreEqual(9, Utils.ToOneBased(8));
+            Assert.AreEqual(0, Utils.ToOneBased(9));    // Hmm...
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToOneBasedTest_Negative()
+        {
+            var value = Utils.ToOneBased(-1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToOneBasedTest_Exceeded()
+        {
+            var value = Utils.ToOneBased(10);
+        }
+
+        [TestMethod()]
+        public void GetEncodingTest()
+        {
+            var utf8 = Utils.GetEncoding(65001);
+            Assert.AreNotEqual(Encoding.UTF8, utf8);
+            Assert.AreEqual(new UTF8Encoding(false), utf8);
+            Assert.AreEqual(Encoding.GetEncoding(932), Utils.GetEncoding(932));
+        }
+    }
+}
