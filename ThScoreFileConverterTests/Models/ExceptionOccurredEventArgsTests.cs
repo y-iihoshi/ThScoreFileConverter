be0f9560725f4d45d782274b70ac0c1dde75181b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2017-11-19 20:21:55 +0900

    Add the unit tests for Models.*EventArgs

---
+++ ThScoreFileConverterTests/Models/ExceptionOccurredEventArgsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ThScoreFileConverter.Models.Tests
+{
+    [TestClass()]
+    public class ExceptionOccurredEventArgsTests
+    {
+        [TestMethod()]
+        public void ExceptionOccurredEventArgsTest()
+        {
+            // NOTE: creating an Exception instance causes CA2201.
+            var ex = new NotImplementedException();
+            var args = new ExceptionOccurredEventArgs(ex);
+            Assert.AreSame(ex, args.Exception);
+        }
+
+        [TestMethod()]
+        public void ExceptionOccurredEventArgsTestNull()
+        {
+            var args = new ExceptionOccurredEventArgs(null);
+            Assert.IsNull(args.Exception);
+        }
+    }
+}
---
+++ ThScoreFileConverterTests/Models/ThConverterEventArgsTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace ThScoreFileConverter.Models.Tests
+{
+    [TestClass()]
+    public class ThConverterEventArgsTests
+    {
+        [TestMethod()]
+        public void ThConverterEventArgsTest()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", 2, 5);
+
+            Assert.AreEqual(@"path\to\file", args.Path);
+            Assert.AreEqual(2, args.Current);
+            Assert.AreEqual(5, args.Total);
+
+            var message = args.Message;
+
+            StringAssert.Contains(message, "file");
+            StringAssert.Contains(message, "2");
+            StringAssert.Contains(message, "5");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThConverterEventArgsTestNullPath()
+        {
+            var args = new ThConverterEventArgs(null, 2, 5);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThConverterEventArgsTestEmptyPath()
+        {
+            var args = new ThConverterEventArgs(string.Empty, 2, 5);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThConverterEventArgsTestNegativeCurrent()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", -1, 5);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThConverterEventArgsTestZeroCurrent()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", 0, 5);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThConverterEventArgsTestExtendedCurrent()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", 6, 5);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThConverterEventArgsTestNegativeTotal()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", 2, -1);
+            Assert.Fail("Unreachable");
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "args")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThConverterEventArgsTestZeroTotal()
+        {
+            var args = new ThConverterEventArgs(@"path\to\file", 2, 0);
+            Assert.Fail("Unreachable");
+        }
+    }
+}
