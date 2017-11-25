571a0a782a4779eac2fef09f351b6d9254463f3b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2017-11-25 19:01:47 +0900

    cleanup

--- ThScoreFileConverterTests/Models/ThConverterEventArgsTests.cs
+++ ThScoreFileConverterTests/Models/ThConverterEventArgsTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 
 namespace ThScoreFileConverter.Models.Tests
 {
