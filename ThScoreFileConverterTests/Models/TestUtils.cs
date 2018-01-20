3e2b5327af3d82460012f7b86cae4d65ad28fd65
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-01-20 10:14:50 +0900

    Revise to use C# 7 type switch

--- ThScoreFileConverterTests/Models/TestUtils.cs
+++ ThScoreFileConverterTests/Models/TestUtils.cs
@@ -23,118 +23,58 @@ namespace ThScoreFileConverter.Models.Tests
 
                     foreach (var arg in args)
                     {
-                        // I want to use C# 7 type switch...
-
-                        var bytesArg = arg as byte[];
-                        if (bytesArg != null)
+                        switch (arg)
                         {
-                            writer.Write(bytesArg);
-                            continue;
-                        }
-
-                        var charsArg = arg as char[];
-                        if (charsArg != null)
-                        {
-                            writer.Write(charsArg);
-                            continue;
-                        }
-
-                        var sbyteArg = arg as sbyte?;
-                        if (sbyteArg != null)
-                        {
-                            writer.Write(sbyteArg.Value);
-                            continue;
-                        }
-
-                        var byteArg = arg as byte?;
-                        if (byteArg != null)
-                        {
-                            writer.Write(byteArg.Value);
-                            continue;
-                        }
-
-                        var charArg = arg as char?;
-                        if (charArg != null)
-                        {
-                            writer.Write(charArg.Value);
-                            continue;
-                        }
-
-                        var shortArg = arg as short?;
-                        if (shortArg != null)
-                        {
-                            writer.Write(shortArg.Value);
-                            continue;
-                        }
-
-                        var ushortArg = arg as ushort?;
-                        if (ushortArg != null)
-                        {
-                            writer.Write(ushortArg.Value);
-                            continue;
-                        }
-
-                        var intArg = arg as int?;
-                        if (intArg != null)
-                        {
-                            writer.Write(intArg.Value);
-                            continue;
-                        }
-
-                        var uintArg = arg as uint?;
-                        if (uintArg != null)
-                        {
-                            writer.Write(uintArg.Value);
-                            continue;
-                        }
-
-                        var longArg = arg as long?;
-                        if (longArg != null)
-                        {
-                            writer.Write(longArg.Value);
-                            continue;
-                        }
-
-                        var ulongArg = arg as ulong?;
-                        if (ulongArg != null)
-                        {
-                            writer.Write(ulongArg.Value);
-                            continue;
-                        }
-
-                        var boolArg = arg as bool?;
-                        if (boolArg != null)
-                        {
-                            writer.Write(boolArg.Value);
-                            continue;
-                        }
-
-                        var doubleArg = arg as double?;
-                        if (doubleArg != null)
-                        {
-                            writer.Write(doubleArg.Value);
-                            continue;
-                        }
-
-                        var floatArg = arg as float?;
-                        if (floatArg != null)
-                        {
-                            writer.Write(floatArg.Value);
-                            continue;
-                        }
-
-                        var stringArg = arg as string;
-                        if (stringArg != null)
-                        {
-                            writer.Write(stringArg);
-                            continue;
-                        }
-
-                        var decimalArg = arg as decimal?;
-                        if (decimalArg != null)
-                        {
-                            writer.Write(decimalArg.Value);
-                            continue;
+                            case byte[] bytesArg:
+                                writer.Write(bytesArg);
+                                break;
+                            case char[] charsArg:
+                                writer.Write(charsArg);
+                                break;
+                            case sbyte sbyteArg:
+                                writer.Write(sbyteArg);
+                                break;
+                            case byte byteArg:
+                                writer.Write(byteArg);
+                                break;
+                            case char charArg:
+                                writer.Write(charArg);
+                                break;
+                            case short shortArg:
+                                writer.Write(shortArg);
+                                break;
+                            case ushort ushortArg:
+                                writer.Write(ushortArg);
+                                break;
+                            case int intArg:
+                                writer.Write(intArg);
+                                break;
+                            case uint uintArg:
+                                writer.Write(uintArg);
+                                break;
+                            case long longArg:
+                                writer.Write(longArg);
+                                break;
+                            case ulong ulongArg:
+                                writer.Write(ulongArg);
+                                break;
+                            case bool boolArg:
+                                writer.Write(boolArg);
+                                break;
+                            case double doubleArg:
+                                writer.Write(doubleArg);
+                                break;
+                            case float floatArg:
+                                writer.Write(floatArg);
+                                break;
+                            case string stringArg:
+                                writer.Write(stringArg);
+                                break;
+                            case decimal decimalArg:
+                                writer.Write(decimalArg);
+                                break;
+                            default:
+                                break;
                         }
                     }
 
