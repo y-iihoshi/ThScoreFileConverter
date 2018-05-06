25493c4dc0f87b18fa76fa2e533da5dca2646d71
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-05-06 23:09:19 +0900

    Start to use OpenCover and Codecov

---
+++ OpenCover.bat
@@ -0,0 +1,14 @@
+set CONFIGURATION=%1
+set OUTPUT=%2
+
+set OPENCOVER=".\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
+set TARGET="%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+set TARGET_ARGS=".\ThScoreFileConverterTests\bin\%CONFIGURATION%\ThScoreFileConverterTests.dll"
+set FILTER="+[*]*"
+
+%OPENCOVER% ^
+    -register:user ^
+    -target:%TARGET% ^
+    -targetargs:%TARGET_ARGS% ^
+    -filter:%FILTER% ^
+    -output:%OUTPUT%
