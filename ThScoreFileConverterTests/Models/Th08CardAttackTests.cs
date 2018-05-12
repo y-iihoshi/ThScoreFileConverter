a15f47b999cd2376005bf2aab90cb4fbb15140a7
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-05-12 22:03:55 +0900

    Revise test coverage of Models.*.CardAttack

--- ThScoreFileConverterTests/Models/Th08CardAttackTests.cs
+++ ThScoreFileConverterTests/Models/Th08CardAttackTests.cs
@@ -203,6 +203,58 @@ namespace ThScoreFileConverter.Models.Tests
         }
 
         [TestMethod()]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void Th08CardAttackTestInvalidLevel()
+        {
+            try
+            {
+                var signature = "CATK";
+                short size1 = 0x22C;
+                short size2 = 0x22C;
+                var unknown1 = 1u;
+                short cardId = 123;
+                byte unknown2 = 2;
+                var level = (Th08Converter.LevelPracticeWithTotal)(-1);
+                var cardName = TestUtils.MakeRandomArray<byte>(0x30);
+                var enemyName = TestUtils.MakeRandomArray<byte>(0x30);
+                var comment = TestUtils.MakeRandomArray<byte>(0x80);
+                var storyMaxBonuses = TestUtils.MakeRandomArray<uint>(13);
+                var storyTrialCounts = TestUtils.MakeRandomArray<int>(12).Concat(new int[] { 1 }).ToArray();
+                var storyClearCounts = TestUtils.MakeRandomArray<int>(13);
+                var practiceMaxBonuses = TestUtils.MakeRandomArray<uint>(13);
+                var practiceTrialCounts = TestUtils.MakeRandomArray<int>(12).Concat(new int[] { 1 }).ToArray();
+                var practiceClearCounts = TestUtils.MakeRandomArray<int>(13);
+                var unknown3 = 3u;
+                var data = TestUtils.MakeByteArray(
+                    unknown1,
+                    (short)(cardId - 1),
+                    unknown2,
+                    (byte)level,
+                    cardName,
+                    enemyName,
+                    comment,
+                    storyMaxBonuses,
+                    storyTrialCounts,
+                    storyClearCounts,
+                    practiceMaxBonuses,
+                    practiceTrialCounts,
+                    practiceClearCounts,
+                    unknown3);
+
+                var chapter = Th06ChapterWrapper<Th08Converter>.Create(
+                    TestUtils.MakeByteArray(signature.ToCharArray(), size1, size2, data));
+                var cardAttack = new Th08CardAttackWrapper(chapter);
+
+                Assert.Fail(TestUtils.Unreachable);
+            }
+            catch (TargetInvocationException ex)
+            {
+                ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                throw;
+            }
+        }
+
+        [TestMethod()]
         public void Th08CardAttackTestNotTried()
         {
             try
