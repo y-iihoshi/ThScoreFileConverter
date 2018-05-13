fc566cdc7448c1e5ede92e8c3c7319e7bd0ceacc
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-05-14 04:08:26 +0900

    Add the unit tests for Models.ThConverterFactory

---
+++ ThScoreFileConverterTests/Models/ThConverterFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThScoreFileConverter.Models.Tests
+{
+    [TestClass()]
+    public class ThConverterFactoryTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            var converter = ThConverterFactory.Create(Properties.Resources.keyTh06);
+
+            Assert.AreEqual(typeof(Th06Converter), converter.GetType());
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "converter")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateTestNull()
+        {
+            var converter = ThConverterFactory.Create(null);
+
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod()]
+        public void CreateTestEmptyKey()
+        {
+            var converter = ThConverterFactory.Create(string.Empty);
+
+            Assert.IsNull(converter);
+        }
+
+        [TestMethod()]
+        public void CreateTestInvalidKey()
+        {
+            var converter = ThConverterFactory.Create("invalidKey");
+
+            Assert.IsNull(converter);
+        }
+    }
+}
