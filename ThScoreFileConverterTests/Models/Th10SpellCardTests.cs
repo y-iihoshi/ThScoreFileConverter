e196c08d9af39d352d4690dae0877914544745cb
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-05 23:00:32 +0900

    Refactor test codes for Models.*.SpellCard

--- ThScoreFileConverterTests/Models/Th10SpellCardTests.cs
+++ ThScoreFileConverterTests/Models/Th10SpellCardTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
@@ -10,18 +11,52 @@ namespace ThScoreFileConverterTests.Models
     [TestClass()]
     public class Th10SpellCardTests
     {
+        internal struct Properties
+        {
+            public byte[] name;
+            public int clearCount;
+            public int trialCount;
+            public int id;
+            public ThConverter.Level level;
+        };
+
+        internal static Properties ValidProperties => new Properties()
+        {
+            name = TestUtils.MakeRandomArray<byte>(0x80),
+            clearCount = 123,
+            trialCount = 456,
+            id = 789,
+            level = ThConverter.Level.Normal
+        };
+
+        internal static byte[] MakeByteArray(in Properties properties)
+        => TestUtils.MakeByteArray(
+            properties.name,
+            properties.clearCount,
+            properties.trialCount,
+            properties.id - 1,
+            (int)properties.level);
+
+        internal static void Validate<TParent>(
+            in Th10SpellCardWrapper<TParent> spellCard, in Properties properties)
+            where TParent : ThConverter
+        {
+            CollectionAssert.AreEqual(properties.name, spellCard.Name?.ToArray());
+            Assert.AreEqual(properties.clearCount, spellCard.ClearCount);
+            Assert.AreEqual(properties.trialCount, spellCard.TrialCount);
+            Assert.AreEqual(properties.id, spellCard.Id);
+            Assert.AreEqual(properties.level, spellCard.Level);
+        }
+
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         internal static void Th10SpellCardTestHelper<TParent>()
             where TParent : ThConverter
             => TestUtils.Wrap(() =>
             {
+                var properties = new Properties();
                 var spellCard = new Th10SpellCardWrapper<TParent>();
 
-                Assert.IsNull(spellCard.Name);
-                Assert.AreEqual(default, spellCard.ClearCount.Value);
-                Assert.AreEqual(default, spellCard.TrialCount.Value);
-                Assert.AreEqual(default, spellCard.Id.Value);
-                Assert.AreEqual(default, spellCard.Level.Value);
+                Validate(spellCard, properties);
                 Assert.IsFalse(spellCard.HasTried().Value);
             });
 
@@ -30,20 +65,11 @@ namespace ThScoreFileConverterTests.Models
             where TParent : ThConverter
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x80);
-                var clearCount = 123;
-                var trialCount = 456;
-                var id = 789;
-                var level = ThConverter.Level.Normal;
+                var properties = ValidProperties;
 
-                var spellCard = Th10SpellCardWrapper<TParent>.Create(
-                    TestUtils.MakeByteArray(name, clearCount, trialCount, id - 1, (int)level));
+                var spellCard = Th10SpellCardWrapper<TParent>.Create(MakeByteArray(properties));
 
-                CollectionAssert.AreEqual(name, spellCard.Name.ToArray());
-                Assert.AreEqual(clearCount, spellCard.ClearCount);
-                Assert.AreEqual(trialCount, spellCard.TrialCount);
-                Assert.AreEqual(id, spellCard.Id);
-                Assert.AreEqual(level, spellCard.Level.Value);
+                Validate(spellCard, properties);
                 Assert.IsTrue(spellCard.HasTried().Value);
             });
 
@@ -54,6 +80,7 @@ namespace ThScoreFileConverterTests.Models
             {
                 var spellCard = new Th10SpellCardWrapper<TParent>();
                 spellCard.ReadFrom(null);
+
                 Assert.Fail(TestUtils.Unreachable);
             });
 
@@ -63,14 +90,10 @@ namespace ThScoreFileConverterTests.Models
             where TParent : ThConverter
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x7F);
-                var clearCount = 123;
-                var trialCount = 456;
-                var id = 789;
-                var level = ThConverter.Level.Normal;
+                var properties = ValidProperties;
+                properties.name = properties.name.Take(properties.name.Length - 1).ToArray();
 
-                var spellCard = Th10SpellCardWrapper<TParent>.Create(
-                    TestUtils.MakeByteArray(name, clearCount, trialCount, id - 1, (int)level));
+                var spellCard = Th10SpellCardWrapper<TParent>.Create(MakeByteArray(properties));
 
                 Assert.Fail(TestUtils.Unreachable);
             });
@@ -80,41 +103,37 @@ namespace ThScoreFileConverterTests.Models
             where TParent : ThConverter
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x81);
-                var clearCount = 123;
-                var trialCount = 456;
-                var id = 789;
-                var level = ThConverter.Level.Normal;
+                var properties = ValidProperties;
+                properties.name = properties.name.Concat(TestUtils.MakeRandomArray<byte>(1)).ToArray();
 
-                var spellCard = Th10SpellCardWrapper<TParent>.Create(
-                    TestUtils.MakeByteArray(name, clearCount, trialCount, id - 1, (int)level));
+                var spellCard = Th10SpellCardWrapper<TParent>.Create(MakeByteArray(properties));
 
-                CollectionAssert.AreNotEqual(name, spellCard.Name.ToArray());
-                CollectionAssert.AreEqual(name.Take(0x80).ToArray(), spellCard.Name.ToArray());
-                Assert.AreNotEqual(clearCount, spellCard.ClearCount);
-                Assert.AreNotEqual(trialCount, spellCard.TrialCount);
-                Assert.AreNotEqual(id, spellCard.Id);
-                Assert.AreNotEqual(level, spellCard.Level.Value);
+                CollectionAssert.AreNotEqual(properties.name, spellCard.Name.ToArray());
+                CollectionAssert.AreEqual(
+                    properties.name.Take(properties.name.Length - 1).ToArray(), spellCard.Name.ToArray());
+                Assert.AreNotEqual(properties.clearCount, spellCard.ClearCount);
+                Assert.AreNotEqual(properties.trialCount, spellCard.TrialCount);
+                Assert.AreNotEqual(properties.id, spellCard.Id);
+                Assert.AreNotEqual(properties.level, spellCard.Level.Value);
                 Assert.IsTrue(spellCard.HasTried().Value);
             });
 
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "spellCard")]
-        internal static void Th10SpellCardReadFromTestInvalidLevelHelper<TParent>()
+        internal static void Th10SpellCardReadFromTestInvalidLevelHelper<TParent>(int level)
             where TParent : ThConverter
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x80);
-                var clearCount = 123;
-                var trialCount = 456;
-                var id = 789;
-                var level = (ThConverter.Level)(-1);
+                var properties = ValidProperties;
+                properties.level = TestUtils.Cast<ThConverter.Level>(level);
 
-                var spellCard = Th10SpellCardWrapper<TParent>.Create(
-                    TestUtils.MakeByteArray(name, clearCount, trialCount, id - 1, (int)level));
+                var spellCard = Th10SpellCardWrapper<TParent>.Create(MakeByteArray(properties));
 
                 Assert.Fail(TestUtils.Unreachable);
             });
+
+        public static IEnumerable<object[]> InvalidLevels
+            => TestUtils.GetInvalidEnumerators(typeof(ThConverter.Level));
 
         #region Th10
 
@@ -140,10 +159,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th10SpellCardReadFromTestExceededName()
             => Th10SpellCardReadFromTestExceededNameHelper<Th10Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th10SpellCardReadFromTestInvalidLevel()
-            => Th10SpellCardReadFromTestInvalidLevelHelper<Th10Converter>();
+        public void Th10SpellCardReadFromTestInvalidLevel(int level)
+            => Th10SpellCardReadFromTestInvalidLevelHelper<Th10Converter>(level);
 
         #endregion
 
@@ -171,10 +192,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th11SpellCardReadFromTestExceededName()
             => Th10SpellCardReadFromTestExceededNameHelper<Th11Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th11SpellCardReadFromTestInvalidLevel()
-            => Th10SpellCardReadFromTestInvalidLevelHelper<Th11Converter>();
+        public void Th11SpellCardReadFromTestInvalidLevel(int level)
+            => Th10SpellCardReadFromTestInvalidLevelHelper<Th11Converter>(level);
 
         #endregion
 
@@ -202,10 +225,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th12SpellCardReadFromTestExceededName()
             => Th10SpellCardReadFromTestExceededNameHelper<Th12Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th12SpellCardReadFromTestInvalidLevel()
-            => Th10SpellCardReadFromTestInvalidLevelHelper<Th12Converter>();
+        public void Th12SpellCardReadFromTestInvalidLevel(int level)
+            => Th10SpellCardReadFromTestInvalidLevelHelper<Th12Converter>(level);
 
         #endregion
     }
--- ThScoreFileConverterTests/Models/Th13SpellCardTests.cs
+++ ThScoreFileConverterTests/Models/Th13SpellCardTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
@@ -10,22 +11,70 @@ namespace ThScoreFileConverterTests.Models
     [TestClass()]
     public class Th13SpellCardTests
     {
+        internal struct Properties<TLevel>
+            where TLevel : struct, Enum
+        {
+            public byte[] name;
+            public int clearCount;
+            public int practiceClearCount;
+            public int trialCount;
+            public int practiceTrialCount;
+            public int id;
+            public TLevel level;
+            public int practiceScore;
+        };
+
+        internal static Properties<TLevel> GetValidProperties<TLevel>()
+            where TLevel : struct, Enum
+            => new Properties<TLevel>()
+            {
+                name = TestUtils.MakeRandomArray<byte>(0x80),
+                clearCount = 1,
+                practiceClearCount = 2,
+                trialCount = 3,
+                practiceTrialCount = 4,
+                id = 5,
+                level = TestUtils.Cast<TLevel>(1),
+                practiceScore = 6789
+            };
+
+        internal static byte[] MakeByteArray<TLevel>(in Properties<TLevel> properties)
+            where TLevel : struct, Enum
+            => TestUtils.MakeByteArray(
+                properties.name,
+                properties.clearCount,
+                properties.practiceClearCount,
+                properties.trialCount,
+                properties.practiceTrialCount,
+                properties.id - 1,
+                TestUtils.Cast<int>(properties.level),
+                properties.practiceScore);
+
+        internal static void Validate<TParent, TLevel>(
+            in Th13SpellCardWrapper<TParent, TLevel> spellCard, in Properties<TLevel> properties)
+            where TParent : ThConverter
+            where TLevel : struct, Enum
+        {
+            CollectionAssert.AreEqual(properties.name, spellCard.Name?.ToArray());
+            Assert.AreEqual(properties.clearCount, spellCard.ClearCount);
+            Assert.AreEqual(properties.practiceClearCount, spellCard.PracticeClearCount);
+            Assert.AreEqual(properties.trialCount, spellCard.TrialCount);
+            Assert.AreEqual(properties.practiceTrialCount, spellCard.PracticeTrialCount);
+            Assert.AreEqual(properties.id, spellCard.Id);
+            Assert.AreEqual(properties.level, spellCard.Level);
+            Assert.AreEqual(properties.practiceScore, spellCard.PracticeScore);
+        }
+
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         internal static void Th13SpellCardTestHelper<TParent, TLevel>()
             where TParent : ThConverter
             where TLevel : struct, Enum
             => TestUtils.Wrap(() =>
             {
+                var properties = new Properties<TLevel>();
                 var spellCard = new Th13SpellCardWrapper<TParent, TLevel>();
 
-                Assert.IsNull(spellCard.Name);
-                Assert.AreEqual(default, spellCard.ClearCount.Value);
-                Assert.AreEqual(default, spellCard.PracticeClearCount.Value);
-                Assert.AreEqual(default, spellCard.TrialCount.Value);
-                Assert.AreEqual(default, spellCard.PracticeTrialCount.Value);
-                Assert.AreEqual(default, spellCard.Id.Value);
-                Assert.AreEqual(default, spellCard.Level.Value);
-                Assert.AreEqual(default, spellCard.PracticeScore.Value);
+                Validate(spellCard, properties);
                 Assert.IsFalse(spellCard.HasTried().Value);
             });
 
@@ -35,34 +84,11 @@ namespace ThScoreFileConverterTests.Models
             where TLevel : struct, Enum
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x80);
-                var clearCount = 123;
-                var practiceClearCount = 234;
-                var trialCount = 456;
-                var practiceTrialCount = 567;
-                var id = 789;
-                var level = 1;
-                var practiceScore = 890;
+                var properties = GetValidProperties<TLevel>();
 
-                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(
-                    TestUtils.MakeByteArray(
-                        name,
-                        clearCount,
-                        practiceClearCount,
-                        trialCount,
-                        practiceTrialCount,
-                        id - 1,
-                        level,
-                        practiceScore));
+                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(MakeByteArray(properties));
 
-                CollectionAssert.AreEqual(name, spellCard.Name.ToArray());
-                Assert.AreEqual(clearCount, spellCard.ClearCount);
-                Assert.AreEqual(practiceClearCount, spellCard.PracticeClearCount);
-                Assert.AreEqual(trialCount, spellCard.TrialCount);
-                Assert.AreEqual(practiceTrialCount, spellCard.PracticeTrialCount);
-                Assert.AreEqual(id, spellCard.Id);
-                Assert.AreEqual(Enum.ToObject(typeof(TLevel), level), spellCard.Level.Value);
-                Assert.AreEqual(practiceScore, spellCard.PracticeScore);
+                Validate(spellCard, properties);
                 Assert.IsTrue(spellCard.HasTried().Value);
             });
 
@@ -73,7 +99,9 @@ namespace ThScoreFileConverterTests.Models
             => TestUtils.Wrap(() =>
             {
                 var spellCard = new Th13SpellCardWrapper<TParent, TLevel>();
+
                 spellCard.ReadFrom(null);
+
                 Assert.Fail(TestUtils.Unreachable);
             });
 
@@ -84,25 +112,10 @@ namespace ThScoreFileConverterTests.Models
             where TLevel : struct, Enum
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x7F);
-                var clearCount = 123;
-                var practiceClearCount = 234;
-                var trialCount = 456;
-                var practiceTrialCount = 567;
-                var id = 789;
-                var level = 1;
-                var practiceScore = 890;
+                var properties = GetValidProperties<TLevel>();
+                properties.name = properties.name.Take(properties.name.Length - 1).ToArray();
 
-                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(
-                    TestUtils.MakeByteArray(
-                        name,
-                        clearCount,
-                        practiceClearCount,
-                        trialCount,
-                        practiceTrialCount,
-                        id - 1,
-                        level,
-                        practiceScore));
+                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(MakeByteArray(properties));
 
                 Assert.Fail(TestUtils.Unreachable);
             });
@@ -113,64 +126,34 @@ namespace ThScoreFileConverterTests.Models
             where TLevel : struct, Enum
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x81);
-                var clearCount = 123;
-                var practiceClearCount = 234;
-                var trialCount = 456;
-                var practiceTrialCount = 567;
-                var id = 789;
-                var level = 1;
-                var practiceScore = 890;
+                var properties = GetValidProperties<TLevel>();
+                properties.name = properties.name.Concat(TestUtils.MakeRandomArray<byte>(1)).ToArray();
 
-                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(
-                    TestUtils.MakeByteArray(
-                        name,
-                        clearCount,
-                        practiceClearCount,
-                        trialCount,
-                        practiceTrialCount,
-                        id - 1,
-                        level,
-                        practiceScore));
+                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(MakeByteArray(properties));
 
-                CollectionAssert.AreNotEqual(name, spellCard.Name.ToArray());
-                CollectionAssert.AreEqual(name.Take(0x80).ToArray(), spellCard.Name.ToArray());
-                Assert.AreNotEqual(clearCount, spellCard.ClearCount);
-                Assert.AreNotEqual(practiceClearCount, spellCard.PracticeClearCount);
-                Assert.AreNotEqual(trialCount, spellCard.TrialCount);
-                Assert.AreNotEqual(practiceTrialCount, spellCard.PracticeTrialCount);
-                Assert.AreNotEqual(id, spellCard.Id);
-                Assert.AreNotEqual(Enum.ToObject(typeof(TLevel), level), spellCard.Level.Value);
-                Assert.AreNotEqual(practiceScore, spellCard.PracticeScore);
+                CollectionAssert.AreNotEqual(properties.name, spellCard.Name.ToArray());
+                CollectionAssert.AreEqual(properties.name.Take(0x80).ToArray(), spellCard.Name.ToArray());
+                Assert.AreNotEqual(properties.clearCount, spellCard.ClearCount);
+                Assert.AreNotEqual(properties.practiceClearCount, spellCard.PracticeClearCount);
+                Assert.AreNotEqual(properties.trialCount, spellCard.TrialCount);
+                Assert.AreNotEqual(properties.practiceTrialCount, spellCard.PracticeTrialCount);
+                Assert.AreNotEqual(properties.id, spellCard.Id);
+                Assert.AreNotEqual(properties.level, spellCard.Level.Value);
+                Assert.AreNotEqual(properties.practiceScore, spellCard.PracticeScore);
                 Assert.IsTrue(spellCard.HasTried().Value);
             });
 
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         [SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "spellCard")]
-        internal static void Th13SpellCardReadFromTestInvalidLevelHelper<TParent, TLevel>()
+        internal static void Th13SpellCardReadFromTestInvalidLevelHelper<TParent, TLevel>(int level)
             where TParent : ThConverter
             where TLevel : struct, Enum
             => TestUtils.Wrap(() =>
             {
-                var name = TestUtils.MakeRandomArray<byte>(0x80);
-                var clearCount = 123;
-                var practiceClearCount = 234;
-                var trialCount = 456;
-                var practiceTrialCount = 567;
-                var id = 789;
-                var level = -1;
-                var practiceScore = 890;
+                var properties = GetValidProperties<TLevel>();
+                properties.level = TestUtils.Cast<TLevel>(level);
 
-                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(
-                    TestUtils.MakeByteArray(
-                        name,
-                        clearCount,
-                        practiceClearCount,
-                        trialCount,
-                        practiceTrialCount,
-                        id - 1,
-                        level,
-                        practiceScore));
+                var spellCard = Th13SpellCardWrapper<TParent, TLevel>.Create(MakeByteArray(properties));
 
                 Assert.Fail(TestUtils.Unreachable);
             });
@@ -201,9 +184,15 @@ namespace ThScoreFileConverterTests.Models
             => Th13SpellCardReadFromTestExceededNameHelper<TParent, ThConverter.Level>();
 
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
-        internal static void Th13SpellCardReadFromTestInvalidLevelHelper<TParent>()
+        internal static void Th13SpellCardReadFromTestInvalidLevelHelper<TParent>(int level)
             where TParent : ThConverter
-            => Th13SpellCardReadFromTestInvalidLevelHelper<TParent, ThConverter.Level>();
+            => Th13SpellCardReadFromTestInvalidLevelHelper<TParent, ThConverter.Level>(level);
+
+        public static IEnumerable<object[]> InvalidTh13LevelPractices
+            => TestUtils.GetInvalidEnumerators(typeof(Th13Converter.LevelPractice));
+
+        public static IEnumerable<object[]> InvalidLevels
+            => TestUtils.GetInvalidEnumerators(typeof(ThConverter.Level));
 
         #region Th13
 
@@ -229,10 +218,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th13SpellCardReadFromTestExceededName()
             => Th13SpellCardReadFromTestExceededNameHelper<Th13Converter, Th13Converter.LevelPractice>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidTh13LevelPractices))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th13SpellCardReadFromTestInvalidLevel()
-            => Th13SpellCardReadFromTestInvalidLevelHelper<Th13Converter, Th13Converter.LevelPractice>();
+        public void Th13SpellCardReadFromTestInvalidLevel(int level)
+            => Th13SpellCardReadFromTestInvalidLevelHelper<Th13Converter, Th13Converter.LevelPractice>(level);
 
         #endregion
 
@@ -260,10 +251,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th14SpellCardReadFromTestExceededName()
             => Th13SpellCardReadFromTestExceededNameHelper<Th14Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th14SpellCardReadFromTestInvalidLevel()
-            => Th13SpellCardReadFromTestInvalidLevelHelper<Th14Converter>();
+        public void Th14SpellCardReadFromTestInvalidLevel(int level)
+            => Th13SpellCardReadFromTestInvalidLevelHelper<Th14Converter>(level);
 
         #endregion
 
@@ -291,10 +284,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th15SpellCardReadFromTestExceededName()
             => Th13SpellCardReadFromTestExceededNameHelper<Th15Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th15SpellCardReadFromTestInvalidLevel()
-            => Th13SpellCardReadFromTestInvalidLevelHelper<Th15Converter>();
+        public void Th15SpellCardReadFromTestInvalidLevel(int level)
+            => Th13SpellCardReadFromTestInvalidLevelHelper<Th15Converter>(level);
 
         #endregion
 
@@ -322,10 +317,12 @@ namespace ThScoreFileConverterTests.Models
         public void Th16SpellCardReadFromTestExceededName()
             => Th13SpellCardReadFromTestExceededNameHelper<Th16Converter>();
 
-        [TestMethod()]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [DataTestMethod]
+        [DynamicData(nameof(InvalidLevels))]
         [ExpectedException(typeof(InvalidCastException))]
-        public void Th16SpellCardReadFromTestInvalidLevel()
-            => Th13SpellCardReadFromTestInvalidLevelHelper<Th16Converter>();
+        public void Th16SpellCardReadFromTestInvalidLevel(int level)
+            => Th13SpellCardReadFromTestInvalidLevelHelper<Th16Converter>(level);
 
         #endregion
     }
