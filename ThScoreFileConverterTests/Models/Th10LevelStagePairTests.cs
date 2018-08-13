d58429f3d48492457a60e4cf060242976dbc4f9b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-14 01:32:59 +0900

    Revise test cavarage of Models.*.LevelStagePair

--- ThScoreFileConverterTests/Models/Th10LevelStagePairTests.cs
+++ ThScoreFileConverterTests/Models/Th10LevelStagePairTests.cs
@@ -25,10 +25,24 @@ namespace ThScoreFileConverterTests.Models
             };
 
         internal static void Validate<TParent, TLevel, TStage>(
-            in Th10LevelStagePairWrapper<TParent, TLevel, TStage> pair, in Properties<TLevel, TStage> properties)
+            in Th10LevelStagePairWrapper<TParent, TLevel, TStage> pair,
+            in Properties<TLevel, TStage> properties)
             where TParent : ThConverter
             where TLevel : struct, Enum
             where TStage : struct, Enum
+        {
+            Assert.AreEqual(properties.level, pair.Level);
+            Assert.AreEqual(properties.stage, pair.Stage);
+        }
+
+        internal static void Validate<TParent, TLevel, TStage, TLevelArg, TStageArg>(
+            in Th10LevelStagePairWrapper<TParent, TLevel, TStage, TLevelArg, TStageArg> pair,
+            in Properties<TLevel, TStage> properties)
+            where TParent : ThConverter
+            where TLevel : struct, Enum
+            where TStage : struct, Enum
+            where TLevelArg : struct, Enum
+            where TStageArg : struct, Enum
         {
             Assert.AreEqual(properties.level, pair.Level);
             Assert.AreEqual(properties.stage, pair.Stage);
@@ -42,7 +56,40 @@ namespace ThScoreFileConverterTests.Models
             {
                 var properties = GetValidProperties<TLevel, TStage>();
 
-                var pair = new Th10LevelStagePairWrapper<TParent, TLevel, TStage>(properties.level, properties.stage);
+                var pair = new Th10LevelStagePairWrapper<TParent, TLevel, TStage>(
+                    properties.level, properties.stage);
+
+                Validate(pair, properties);
+            });
+
+        internal static void LevelStagePairTestHelper<TParent, TLevel, TStage, TLevelArg, TStageArg>()
+            where TParent : ThConverter
+            where TLevel : struct, Enum
+            where TStage : struct, Enum
+            where TLevelArg : struct, Enum
+            where TStageArg : struct, Enum
+            => TestUtils.Wrap(() =>
+            {
+                var properties = GetValidProperties<TLevel, TStage>();
+
+                var pair = new Th10LevelStagePairWrapper<TParent, TLevel, TStage, TLevelArg, TStageArg>(
+                    properties.level, properties.stage);
+
+                Validate(pair, properties);
+            });
+
+        internal static void LevelStagePairTestCastHelper<TParent, TLevel, TStage, TLevelArg, TStageArg>()
+            where TParent : ThConverter
+            where TLevel : struct, Enum
+            where TStage : struct, Enum
+            where TLevelArg : struct, Enum
+            where TStageArg : struct, Enum
+            => TestUtils.Wrap(() =>
+            {
+                var properties = GetValidProperties<TLevel, TStage>();
+
+                var pair = new Th10LevelStagePairWrapper<TParent, TLevel, TStage, TLevelArg, TStageArg>(
+                    TestUtils.Cast<TLevelArg>(properties.level), TestUtils.Cast<TStageArg>(properties.stage));
 
                 Validate(pair, properties);
             });
@@ -65,14 +112,56 @@ namespace ThScoreFileConverterTests.Models
 
         [TestMethod]
         public void Th14LevelStagePairTest()
-            => LevelStagePairTestHelper<Th14Converter, Th14Converter.LevelPractice, Th14Converter.StagePractice>();
+            => LevelStagePairTestHelper<
+                Th14Converter,
+                Th14Converter.LevelPractice,
+                Th14Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
+
+        [TestMethod]
+        public void Th14LevelStagePairTestCast()
+            => LevelStagePairTestCastHelper<
+                Th14Converter,
+                Th14Converter.LevelPractice,
+                Th14Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
 
         [TestMethod]
         public void Th15LevelStagePairTest()
-            => LevelStagePairTestHelper<Th15Converter, ThConverter.Level, Th15Converter.StagePractice>();
+            => LevelStagePairTestHelper<
+                Th15Converter,
+                ThConverter.Level,
+                Th15Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
+
+        [TestMethod]
+        public void Th15LevelStagePairTestCast()
+            => LevelStagePairTestCastHelper<
+                Th15Converter,
+                ThConverter.Level,
+                Th15Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
 
         [TestMethod]
         public void Th16LevelStagePairTest()
-            => LevelStagePairTestHelper<Th16Converter, ThConverter.Level, Th16Converter.StagePractice>();
+            => LevelStagePairTestHelper<
+                Th16Converter,
+                ThConverter.Level,
+                Th16Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
+
+        [TestMethod]
+        public void Th16LevelStagePairTestCast()
+            => LevelStagePairTestCastHelper<
+                Th16Converter,
+                ThConverter.Level,
+                Th16Converter.StagePractice,
+                ThConverter.Level,
+                ThConverter.Stage>();
     }
 }
--- ThScoreFileConverterTests/Models/Th10LevelStagePairWrapper.cs
+++ ThScoreFileConverterTests/Models/Th10LevelStagePairWrapper.cs
@@ -26,4 +26,31 @@ namespace ThScoreFileConverterTests.Models
         public TStage? Stage
             => this.pobj.GetProperty(nameof(this.Stage)) as TStage?;
     }
+
+    // NOTE: Setting the accessibility as public causes CS0703.
+    internal sealed class Th10LevelStagePairWrapper<TParent, TLevel, TStage, TLevelArg, TStageArg>
+        where TParent : ThConverter
+        where TLevel : struct, Enum
+        where TStage : struct, Enum
+        where TLevelArg : struct, Enum
+        where TStageArg : struct, Enum
+    {
+        private static Type ParentType = typeof(TParent);
+        private static string AssemblyNameToTest = ParentType.Assembly.GetName().Name;
+        private static string TypeNameToTest = ParentType.FullName + "+LevelStagePair";
+
+        private readonly PrivateObject pobj = null;
+
+        public Th10LevelStagePairWrapper(TLevel level, TStage stage)
+            => this.pobj = new PrivateObject(AssemblyNameToTest, TypeNameToTest, new object[] { level, stage });
+        public Th10LevelStagePairWrapper(TLevelArg level, TStageArg stage)
+            => this.pobj = new PrivateObject(AssemblyNameToTest, TypeNameToTest, new object[] { level, stage });
+
+        public object Target
+            => this.pobj.Target;
+        public TLevel? Level
+            => this.pobj.GetProperty(nameof(this.Level)) as TLevel?;
+        public TStage? Stage
+            => this.pobj.GetProperty(nameof(this.Stage)) as TStage?;
+    }
 }
