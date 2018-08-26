95f1fc94e8bca853968f8a44cf60e1bb9466e914
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-26 16:45:19 +0900

    Add the unit tests for Models.Th125Converter.Detail

---
+++ ThScoreFileConverterTests/Models/Th125DetailTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverterTests.Models.Wrappers;
+
+namespace ThScoreFileConverterTests.Models
+{
+    [TestClass]
+    public class Th125DetailTests
+    {
+        internal struct Properties
+        {
+            public bool outputs;
+            public string format;
+            public string value;
+        };
+
+        internal static Properties ValidProperties => new Properties()
+        {
+            outputs = true,
+            format = "Cat Bonus     {0,9}",
+            value = "+ 666"
+        };
+
+        internal static void Validate(in Th125DetailWrapper spellCardInfo, in Properties properties)
+        {
+            Assert.AreEqual(properties.outputs, spellCardInfo.Outputs);
+            Assert.AreEqual(properties.format, spellCardInfo.Format);
+            Assert.AreEqual(properties.value, spellCardInfo.Value);
+        }
+
+        [TestMethod]
+        public void Th125DetailTest() => TestUtils.Wrap(() =>
+        {
+            var properties = ValidProperties;
+
+            var spellCardInfo = new Th125DetailWrapper(properties.outputs, properties.format, properties.value);
+
+            Validate(spellCardInfo, properties);
+        });
+    }
+}
---
+++ ThScoreFileConverterTests/Models/Wrappers/Th125DetailWrapper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ThScoreFileConverter.Models;
+
+namespace ThScoreFileConverterTests.Models.Wrappers
+{
+    public sealed class Th125DetailWrapper
+    {
+        private static Type ParentType = typeof(Th125Converter);
+        private static string AssemblyNameToTest = ParentType.Assembly.GetName().Name;
+        private static string TypeNameToTest = ParentType.FullName + "+Detail";
+
+        private readonly PrivateObject pobj = null;
+
+        public Th125DetailWrapper(bool outputs, string format, string value)
+            => this.pobj = new PrivateObject(
+                AssemblyNameToTest, TypeNameToTest, new object[] { outputs, format, value });
+
+        public object Target
+            => this.pobj.Target;
+        public bool? Outputs
+            => this.pobj.GetProperty(nameof(Outputs)) as bool?;
+        public string Format
+            => this.pobj.GetProperty(nameof(Format)) as string;
+        public string Value
+            => this.pobj.GetProperty(nameof(Value)) as string;
+    }
+}
