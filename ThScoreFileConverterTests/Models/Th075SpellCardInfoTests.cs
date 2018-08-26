eca818d5a2e1aaa12cbf544063f72cd1aace54fa
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-26 16:31:11 +0900

    Add the unit tests for Models.Th075Converter.SpellCardInfo

---
+++ ThScoreFileConverterTests/Models/Th075SpellCardInfoTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverterTests.Models.Wrappers;
+
+namespace ThScoreFileConverterTests.Models
+{
+    [TestClass]
+    public class Th075SpellCardInfoTests
+    {
+        internal struct Properties
+        {
+            public string name;
+            public Th075Converter.Chara enemy;
+            public Th075Converter.Level level;
+        };
+
+        internal static Properties ValidProperties => new Properties()
+        {
+            name = "「百万鬼夜行」",
+            enemy = Th075Converter.Chara.Suika,
+            level = Th075Converter.Level.Normal
+        };
+
+        internal static void Validate(in Th075SpellCardInfoWrapper spellCardInfo, in Properties properties)
+        {
+            Assert.AreEqual(properties.name, spellCardInfo.Name);
+            Assert.AreEqual(properties.enemy, spellCardInfo.Enemy);
+            Assert.AreEqual(properties.level, spellCardInfo.Level);
+        }
+
+        [TestMethod]
+        public void Th075SpellCardInfoTest() => TestUtils.Wrap(() =>
+        {
+            var properties = ValidProperties;
+
+            var spellCardInfo = new Th075SpellCardInfoWrapper(properties.name, properties.enemy, properties.level);
+
+            Validate(spellCardInfo, properties);
+        });
+    }
+}
---
+++ ThScoreFileConverterTests/Models/Wrappers/Th075SpellCardInfoWrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using ThScoreFileConverter.Models;
+
+namespace ThScoreFileConverterTests.Models.Wrappers
+{
+    // NOTE: Setting the accessibility as public causes CS0051 and CS0053.
+    internal sealed class Th075SpellCardInfoWrapper
+    {
+        private static Type ParentType = typeof(Th075Converter);
+        private static string AssemblyNameToTest = ParentType.Assembly.GetName().Name;
+        private static string TypeNameToTest = ParentType.FullName + "+SpellCardInfo";
+
+        private readonly PrivateObject pobj = null;
+
+        public Th075SpellCardInfoWrapper(string name, Th075Converter.Chara enemy, Th075Converter.Level level)
+            => this.pobj = new PrivateObject(AssemblyNameToTest, TypeNameToTest, new object[] { name, enemy, level });
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public object Target
+            => this.pobj.Target;
+        public string Name
+            => this.pobj.GetProperty(nameof(Name)) as string;
+        public Th075Converter.Chara? Enemy
+            => this.pobj.GetProperty(nameof(Enemy)) as Th075Converter.Chara?;
+        public Th075Converter.Level? Level
+            => this.pobj.GetProperty(nameof(Level)) as Th075Converter.Level?;
+    }
+}
