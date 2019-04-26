98da17e786dd12d5bec721ce9a09093296ed0299
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-04-26 13:14:51 +0900

    Add a PowerShell script to confirm the test envirionment

---
+++ PrintTestEnvironment.ps1
@@ -0,0 +1,14 @@
+echo System.Text.Encoding.Default
+[System.Text.Encoding]::Default
+
+echo System.Text.Encoding.ASCII
+[System.Text.Encoding]::ASCII
+
+echo System.Text.Encoding.UTF8
+[System.Text.Encoding]::UTF8
+
+echo System.Globalization.CultureInfo.CurrentCulture.TextInfo
+[System.Globalization.CultureInfo]::CurrentCulture.TextInfo
+
+echo System.Globalization.CultureInfo.InvariantCulture.TextInfo
+[System.Globalization.CultureInfo]::InvariantCulture.TextInfo
