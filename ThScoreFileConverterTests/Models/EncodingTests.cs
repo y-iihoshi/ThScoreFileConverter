8270ec9e1c34a9866f8ff4e5091337c57cb58d2f
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-04-26 19:05:23 +0900

    Add unit tests for Models.Encoding

---
+++ ThScoreFileConverterTests/Models/EncodingTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+
+namespace ThScoreFileConverterTests.Models
+{
+    [TestClass]
+    public class EncodingTests
+    {
+        [TestMethod]
+        public void CP932Test()
+        {
+            Assert.AreEqual(TestUtils.CP932Encoding, Encoding.CP932);
+        }
+
+        [TestMethod]
+        public void DefaultTest()
+        {
+            Assert.AreEqual(System.Text.Encoding.Default, Encoding.Default);
+        }
+
+        [TestMethod]
+        public void UTF8Test()
+        {
+            Assert.AreNotEqual(System.Text.Encoding.UTF8, Encoding.UTF8);
+            Assert.AreEqual(new System.Text.UTF8Encoding(false), Encoding.UTF8);
+        }
+    }
+}
