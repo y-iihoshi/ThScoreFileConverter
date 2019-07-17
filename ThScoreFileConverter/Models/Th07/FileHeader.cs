b14cc2f50eed32467f86e047669b521a2519d0da
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-07-17 22:05:51 +0900

    Refactor by adding Models.Th07.FileHeader

---
+++ ThScoreFileConverter/Models/Th07/FileHeader.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileHeader.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#pragma warning disable SA1600 // Elements should be documented
+
+namespace ThScoreFileConverter.Models.Th07
+{
+    using System;
+    using System.IO;
+
+    internal class FileHeader : IBinaryReadable, IBinaryWritable
+    {
+        private ushort unknown1;
+        private ushort unknown2;
+        private uint unknown3;
+
+        public FileHeader()
+        {
+        }
+
+        public ushort Checksum { get; private set; }
+
+        public short Version { get; private set; }
+
+        public int Size { get; private set; }
+
+        public int DecodedAllSize { get; private set; }
+
+        public int DecodedBodySize { get; private set; }
+
+        public int EncodedBodySize { get; private set; }
+
+        public virtual bool IsValid => this.DecodedAllSize == this.Size + this.DecodedBodySize;
+
+        public void ReadFrom(BinaryReader reader)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+
+            this.unknown1 = reader.ReadUInt16();
+            this.Checksum = reader.ReadUInt16();
+            this.Version = reader.ReadInt16();
+            this.unknown2 = reader.ReadUInt16();
+            this.Size = reader.ReadInt32();
+            this.unknown3 = reader.ReadUInt32();
+            this.DecodedAllSize = reader.ReadInt32();
+            this.DecodedBodySize = reader.ReadInt32();
+            this.EncodedBodySize = reader.ReadInt32();
+        }
+
+        public void WriteTo(BinaryWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+
+            writer.Write(this.unknown1);
+            writer.Write(this.Checksum);
+            writer.Write(this.Version);
+            writer.Write(this.unknown2);
+            writer.Write(this.Size);
+            writer.Write(this.unknown3);
+            writer.Write(this.DecodedAllSize);
+            writer.Write(this.DecodedBodySize);
+            writer.Write(this.EncodedBodySize);
+        }
+    }
+}
--- ThScoreFileConverter/Models/Th08Converter.cs
+++ ThScoreFileConverter/Models/Th08Converter.cs
@@ -1129,72 +1129,12 @@ namespace ThScoreFileConverter.Models
             }
         }
 
-        private class FileHeader : IBinaryReadable, IBinaryWritable
+        private class FileHeader : Th07.FileHeader
         {
             public const short ValidVersion = 0x0001;
             public const int ValidSize = 0x0000001C;
 
-            private ushort unknown1;
-            private ushort unknown2;
-            private uint unknown3;
-
-            public FileHeader()
-            {
-            }
-
-            public ushort Checksum { get; private set; }
-
-            public short Version { get; private set; }
-
-            public int Size { get; private set; }
-
-            public int DecodedAllSize { get; private set; }
-
-            public int DecodedBodySize { get; private set; }
-
-            public int EncodedBodySize { get; private set; }
-
-            public bool IsValid
-            {
-                get
-                {
-                    return (this.Version == ValidVersion)
-                        && (this.Size == ValidSize)
-                        && (this.DecodedAllSize == this.Size + this.DecodedBodySize);
-                }
-            }
-
-            public void ReadFrom(BinaryReader reader)
-            {
-                if (reader == null)
-                    throw new ArgumentNullException(nameof(reader));
-
-                this.unknown1 = reader.ReadUInt16();
-                this.Checksum = reader.ReadUInt16();
-                this.Version = reader.ReadInt16();
-                this.unknown2 = reader.ReadUInt16();
-                this.Size = reader.ReadInt32();
-                this.unknown3 = reader.ReadUInt32();
-                this.DecodedAllSize = reader.ReadInt32();
-                this.DecodedBodySize = reader.ReadInt32();
-                this.EncodedBodySize = reader.ReadInt32();
-            }
-
-            public void WriteTo(BinaryWriter writer)
-            {
-                if (writer == null)
-                    throw new ArgumentNullException(nameof(writer));
-
-                writer.Write(this.unknown1);
-                writer.Write(this.Checksum);
-                writer.Write(this.Version);
-                writer.Write(this.unknown2);
-                writer.Write(this.Size);
-                writer.Write(this.unknown3);
-                writer.Write(this.DecodedAllSize);
-                writer.Write(this.DecodedBodySize);
-                writer.Write(this.EncodedBodySize);
-            }
+            public override bool IsValid => base.IsValid && (this.Version == ValidVersion) && (this.Size == ValidSize);
         }
 
         private class AllScoreData
