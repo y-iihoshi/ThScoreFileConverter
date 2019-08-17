9a96e7f6d66a22248e0a9db8063a94995617c8be
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-08-17 21:10:33 +0900

    Delete Models.Th13.PracticeWrapper which became meaningless

--- ThScoreFileConverterTests/Models/Th13/PracticeTests.cs
+++ ThScoreFileConverterTests/Models/Th13/PracticeTests.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 using ThScoreFileConverter.Models.Th13;
-using ThScoreFileConverterTests.Models.Th13.Wrappers;
 
 namespace ThScoreFileConverterTests.Models.Th13
 {
@@ -25,8 +25,27 @@ namespace ThScoreFileConverterTests.Models.Th13
         internal static byte[] MakeByteArray(in Properties properties)
             => TestUtils.MakeByteArray(properties.score, properties.clearFlag, properties.enableFlag, (ushort)0);
 
-        internal static void Validate(in PracticeWrapper practice, in Properties properties)
-            => Validate(practice.Target as Practice, properties);
+        internal static Practice Create(byte[] array)
+        {
+            var practice = new Practice();
+
+            MemoryStream stream = null;
+            try
+            {
+                stream = new MemoryStream(array);
+                using (var reader = new BinaryReader(stream))
+                {
+                    stream = null;
+                    practice.ReadFrom(reader);
+                }
+            }
+            finally
+            {
+                stream?.Dispose();
+            }
+
+            return practice;
+        }
 
         internal static void Validate(in Practice practice, in Properties properties)
         {
@@ -40,7 +59,7 @@ namespace ThScoreFileConverterTests.Models.Th13
             => TestUtils.Wrap(() =>
             {
                 var properties = new Properties();
-                var practice = new PracticeWrapper();
+                var practice = new Practice();
 
                 Validate(practice, properties);
             });
@@ -49,7 +68,7 @@ namespace ThScoreFileConverterTests.Models.Th13
         public void Th13PracticeReadFromTest()
             => TestUtils.Wrap(() =>
             {
-                var practice = PracticeWrapper.Create(MakeByteArray(ValidProperties));
+                var practice = Create(MakeByteArray(ValidProperties));
 
                 Validate(practice, ValidProperties);
             });
@@ -59,7 +78,7 @@ namespace ThScoreFileConverterTests.Models.Th13
         public void Th13PracticeReadFromTestNull()
             => TestUtils.Wrap(() =>
             {
-                var practice = new PracticeWrapper();
+                var practice = new Practice();
 
                 practice.ReadFrom(null);
 
