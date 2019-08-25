439c75529acae80bb8a5b925b087172ed3d3eac9
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-08-25 15:11:51 +0900

    Modify T4 templates for Th06 to use public enums

---
+++ ThScoreFileConverter/Models/EnumerableExtensions.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EnumerableExtensions.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThScoreFileConverter.Models
+{
+    /// <summary>
+    /// Provides some extension methods for <see cref="IEnumerable{T}"/> types.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Bypasses a specified number of elements from the end of a sequence and then returns the remaining elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to return elements from.</param>
+        /// <param name="count">The number of elements to skip before returning the remaining elements.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> that contains the elements that occur before the specified index in the
+        /// input sequence.
+        /// </returns>
+        public static IEnumerable<TSource> SkipLast<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.Take(source.Count() - count);
+        }
+
+        /// <summary>
+        /// Returns a specified number of contiguous elements from the end of a sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence to return elements from.</param>
+        /// <param name="count">The number of elements to return.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> that contains the specified number of elements from the end of the input
+        /// sequence.
+        /// </returns>
+        public static IEnumerable<TSource> TakeLast<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.Skip(source.Count() - count);
+        }
+    }
+}
---
+++ ThScoreFileConverterTests/Models/EnumerableExtensionsTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+
+namespace ThScoreFileConverterTests.Models
+{
+    [TestClass]
+    public class EnumerableExtensionsTests
+    {
+        [TestMethod]
+        public void SkipLastTest()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var expected = array.Take(3).ToArray();
+            var actual = array.SkipLast(2).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SkipLastTestNull()
+        {
+            int[] array = null;
+            _ = array.SkipLast(2);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void SkipLastTestNegative()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var actual = array.SkipLast(-1).ToArray();
+            CollectionAssert.AreEqual(array, actual);
+        }
+
+        [TestMethod]
+        public void SkipLastTestZero()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var actual = array.SkipLast(0).ToArray();
+            CollectionAssert.AreEqual(array, actual);
+        }
+
+        [TestMethod]
+        public void SkipLastTestExceeded()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var expected = new int[] { };
+            var actual = array.SkipLast(array.Length + 1).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TakeLastTest()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var expected = array.Skip(3).ToArray();
+            var actual = array.TakeLast(2).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TakeLastTestNull()
+        {
+            int[] array = null;
+            _ = array.TakeLast(2);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void TakeLastTestNegative()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var expected = new int[] { };
+            var actual = array.TakeLast(-1).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TakeLastTestZero()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var expected = new int[] { };
+            var actual = array.TakeLast(0).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TakeLastTestExceeded()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var actual = array.TakeLast(array.Length + 1).ToArray();
+            CollectionAssert.AreEqual(array, actual);
+        }
+    }
+}
