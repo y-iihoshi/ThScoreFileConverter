d81f5ebb5b4e16676e2ca0f2e27dd1af8a4c5b2c
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-09-09 03:00:43 +0900

    Add test cases for Models.Th07.PlayReplacer

---
+++ ThScoreFileConverterTests/Models/Th07/PlayReplacerTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models.Th07;
+using ThScoreFileConverterTests.Models.Th06.Wrappers;
+
+namespace ThScoreFileConverterTests.Models.Th07
+{
+    [TestClass]
+    public class PlayReplacerTests
+    {
+        internal static PlayStatus PlayStatus { get; } = new PlayStatus(
+            ChapterWrapper.Create(PlayStatusTests.MakeByteArray(PlayStatusTests.ValidProperties)).Target);
+
+        [TestMethod]
+        public void PlayReplacerTest()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlayReplacerTestNull()
+        {
+            _ = new PlayReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("3", replacer.Replace("%T07PLAYHMB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotal()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("3", replacer.Replace("%T07PLAYTMB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotal()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("1", replacer.Replace("%T07PLAYHTL"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestClearCount()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("3", replacer.Replace("%T07PLAYHCL"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestContinueCount()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("4", replacer.Replace("%T07PLAYHCN"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestPracticeCount()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("5", replacer.Replace("%T07PLAYHPR"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestRetryCount()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("2", replacer.Replace("%T07PLAYHRT"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("%T07XXXXHMB", replacer.Replace("%T07XXXXHMB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("%T07PLAYYMB", replacer.Replace("%T07PLAYYMB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new PlayReplacer(PlayStatus);
+            Assert.AreEqual("%T07PLAYHXX", replacer.Replace("%T07PLAYHXX"));
+        }
+    }
+}
