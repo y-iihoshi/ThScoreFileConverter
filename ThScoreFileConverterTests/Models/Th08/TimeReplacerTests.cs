9e7ea83be59cc4f65e784efd7ea2e5add3e592cf
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-10-14 20:13:11 +0900

    Add test cases for Models.Th08.TimeReplacer

---
+++ ThScoreFileConverterTests/Models/Th08/TimeReplacerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models.Th08;
+using ThScoreFileConverterTests.Models.Th08.Stubs;
+
+namespace ThScoreFileConverterTests.Models.Th08
+{
+    [TestClass]
+    public class TimeReplacerTests
+    {
+        internal static IPlayStatus PlayStatus { get; } = new PlayStatusStub(PlayStatusTests.ValidStub);
+
+        [TestMethod]
+        public void TimeReplacerTest()
+        {
+            var replacer = new TimeReplacer(PlayStatus);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TimeReplacerTestNull()
+        {
+            _ = new TimeReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ReplaceTestPlay()
+        {
+            var replacer = new TimeReplacer(PlayStatus);
+            Assert.AreEqual("23:45:19.876", replacer.Replace("%T08TIMEPLY"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestAll()
+        {
+            var replacer = new TimeReplacer(PlayStatus);
+            Assert.AreEqual("12:34:56.789", replacer.Replace("%T08TIMEALL"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new TimeReplacer(PlayStatus);
+            Assert.AreEqual("%T08XXXXPLY", replacer.Replace("%T08XXXXPLY"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new TimeReplacer(PlayStatus);
+            Assert.AreEqual("%T08TIMEXXX", replacer.Replace("%T08TIMEXXX"));
+        }
+    }
+}
