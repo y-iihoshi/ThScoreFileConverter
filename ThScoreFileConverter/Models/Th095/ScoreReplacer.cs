08236e6406d8e8fa1cfbca11a61141539a3e85bd
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-10-19 22:31:34 +0900

    Add test cases for Models.Th09.ScoreReplacer

--- ThScoreFileConverter/Models/Th095/ScoreReplacer.cs
+++ ThScoreFileConverter/Models/Th095/ScoreReplacer.cs
@@ -7,6 +7,7 @@
 
 #pragma warning disable SA1600 // Elements should be documented
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -24,6 +25,9 @@ namespace ThScoreFileConverter.Models.Th095
 
         public ScoreReplacer(IReadOnlyList<IScore> scores)
         {
+            if (scores is null)
+                throw new ArgumentNullException(nameof(scores));
+
             this.evaluator = new MatchEvaluator(match =>
             {
                 var level = Parsers.LevelParser.Parse(match.Groups[1].Value);
---
+++ ThScoreFileConverterTests/Models/Th095/ScoreReplacerTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter;
+using ThScoreFileConverter.Models.Th095;
+using ThScoreFileConverterTests.Models.Th095.Stubs;
+
+namespace ThScoreFileConverterTests.Models.Th095
+{
+    [TestClass]
+    public class ScoreReplacerTests
+    {
+        internal static IReadOnlyList<IScore> Scores { get; } = new List<IScore>
+        {
+            new ScoreStub(ScoreTests.ValidStub),
+        };
+
+        [TestMethod]
+        public void ScoreReplacerTest()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ScoreReplacerTestNull()
+        {
+            _ = new ScoreReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ScoreReplacerTestEmpty()
+        {
+            var scores = new List<IScore>();
+            var replacer = new ScoreReplacer(scores);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestHighScore()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new ScoreReplacer(Scores);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("1,234,567", replacer.Replace("%T95SCR961"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("1234567", replacer.Replace("%T95SCR961"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestBestShotScore()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new ScoreReplacer(Scores);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("23,456", replacer.Replace("%T95SCR962"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("23456", replacer.Replace("%T95SCR962"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestTrialCount()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new ScoreReplacer(Scores);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("9,876", replacer.Replace("%T95SCR963"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("9876", replacer.Replace("%T95SCR963"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestSlowRate()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("2.340%", replacer.Replace("%T95SCR964"));  // really...?
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var scores = new List<IScore>();
+            var replacer = new ScoreReplacer(scores);
+            Assert.AreEqual("0", replacer.Replace("%T95SCR961"));
+            Assert.AreEqual("0", replacer.Replace("%T95SCR962"));
+            Assert.AreEqual("0", replacer.Replace("%T95SCR963"));
+            Assert.AreEqual("-----%", replacer.Replace("%T95SCR964"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestNonexistentLevel()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("0", replacer.Replace("%T95SCR861"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestNonexistentScene()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("0", replacer.Replace("%T95SCR951"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestNonexistentSpellCard()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("%T95SCR991", replacer.Replace("%T95SCR991"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("%T95XXX961", replacer.Replace("%T95XXX961"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("%T95SCRY61", replacer.Replace("%T95SCRY61"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidScene()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("%T95SCR9X1", replacer.Replace("%T95SCR9X1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new ScoreReplacer(Scores);
+            Assert.AreEqual("%T95SCR96X", replacer.Replace("%T95SCR96X"));
+        }
+    }
+}
