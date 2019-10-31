e4c1c4667d9a90093bc755a63d89283f902a0838
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-10-31 21:23:47 +0900

    Add test cases for Models.Th10.ClearReplacer

---
+++ ThScoreFileConverterTests/Models/Th10/ClearReplacerTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th10;
+using ThScoreFileConverterTests.Models.Th10.Stubs;
+using IClearData = ThScoreFileConverter.Models.Th10.IClearData<
+    ThScoreFileConverter.Models.Th10.CharaWithTotal, ThScoreFileConverter.Models.Th10.StageProgress>;
+
+namespace ThScoreFileConverterTests.Models.Th10
+{
+    [TestClass]
+    public class ClearReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Rankings = Utils.GetEnumerator<Level>().ToDictionary(
+                        level => level,
+                        level => Enumerable.Range(0, 10).Select(
+                            index => new ScoreDataStub<StageProgress>()
+                            {
+                                StageProgress = (level == Level.Extra)
+                                    ? StageProgress.Extra : (StageProgress)(5 - (index % 5)),
+                                DateTime = (uint)index % 2,
+                            }).ToList() as IReadOnlyList<IScoreData<StageProgress>>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void ClearReplacerTest()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ClearReplacerTestNull()
+        {
+            _ = new ClearReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ClearReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new ClearReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.AreEqual("Stage 5", replacer.Replace("%T10CLEARHRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestExtra()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.AreEqual("Not Clear", replacer.Replace("%T10CLEARXRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new ClearReplacer(dictionary);
+            Assert.AreEqual("-------", replacer.Replace("%T10CLEARHRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyRankings()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Rankings = new Dictionary<Level, IReadOnlyList<IScoreData<StageProgress>>>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ClearReplacer(dictionary);
+            Assert.AreEqual("-------", replacer.Replace("%T10CLEARHRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyRanking()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Rankings = Utils.GetEnumerator<Level>().ToDictionary(
+                        level => level,
+                        level => new List<IScoreData<StageProgress>>() as IReadOnlyList<IScoreData<StageProgress>>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ClearReplacer(dictionary);
+            Assert.AreEqual("-------", replacer.Replace("%T10CLEARHRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10XXXXXHRB", replacer.Replace("%T10XXXXXHRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CLEARYRB", replacer.Replace("%T10CLEARYRB"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new ClearReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CLEARHXX", replacer.Replace("%T10CLEARHXX"));
+        }
+    }
+}
