a149f8fb42c164a538660d5d2d4e36ad57aafa08
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-10-31 20:53:06 +0900

    Add test cases for Models.Th10.CollectRateReplacer

---
+++ ThScoreFileConverterTests/Models/Th10/CollectRateReplacerTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th10;
+using ThScoreFileConverterTests.Models.Th10.Stubs;
+using IClearData = ThScoreFileConverter.Models.Th10.IClearData<
+    ThScoreFileConverter.Models.Th10.CharaWithTotal, ThScoreFileConverter.Models.Th10.StageProgress>;
+
+namespace ThScoreFileConverterTests.Models.Th10
+{
+    [TestClass]
+    public class CollectRateReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Cards = Definitions.CardTable.ToDictionary(
+                        pair => pair.Key,
+                        pair => new SpellCardStub
+                        {
+                            ClearCount = pair.Key % 3,
+                            TrialCount = pair.Key % 5,
+                            Id = pair.Value.Id,
+                            Level = pair.Value.Level,
+                        } as ISpellCard<Level>),
+                },
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.Total,
+                    Cards = Definitions.CardTable.ToDictionary(
+                        pair => pair.Key,
+                        pair => new SpellCardStub
+                        {
+                            ClearCount = pair.Key % 7,
+                            TrialCount = pair.Key % 11,
+                            Id = pair.Value.Id,
+                            Level = pair.Value.Level,
+                        } as ISpellCard<Level>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void CollectRateReplacerTest()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectRateReplacerTestNull()
+        {
+            _ = new CollectRateReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void CollectRateReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CollectRateReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("3", replacer.Replace("%T10CRGHRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("4", replacer.Replace("%T10CRGHRB32"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelExtraClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("9", replacer.Replace("%T10CRGXRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelExtraTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("10", replacer.Replace("%T10CRGXRB32"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotalClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("10", replacer.Replace("%T10CRGTRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotalTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("13", replacer.Replace("%T10CRGTRB32"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotalClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("4", replacer.Replace("%T10CRGHTL31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotalTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("3", replacer.Replace("%T10CRGHTL32"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStageTotalClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("17", replacer.Replace("%T10CRGHRB01"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStageTotalTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("20", replacer.Replace("%T10CRGHRB02"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalClearCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("95", replacer.Replace("%T10CRGTTL01"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalTrialCount()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("100", replacer.Replace("%T10CRGTTL02"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CollectRateReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T10CRGHRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyCards()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Cards = new Dictionary<int, ISpellCard<Level>>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new CollectRateReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T10CRGHRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10XXXHRB31", replacer.Replace("%T10XXXHRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CRGYRB31", replacer.Replace("%T10CRGYRB31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CRGHXX31", replacer.Replace("%T10CRGHXX31"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidStage()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CRGHRBX1", replacer.Replace("%T10CRGHRBX1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new CollectRateReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T10CRGHRB3X", replacer.Replace("%T10CRGHRB3X"));
+        }
+    }
+}
