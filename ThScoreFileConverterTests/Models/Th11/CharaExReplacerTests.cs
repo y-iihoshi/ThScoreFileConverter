cfd9bd826e490909c04d33848f2b257d144b6ca0
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-03 16:39:37 +0900

    Add test cases for Models.Th11.CharaExReplacer

---
+++ ThScoreFileConverterTests/Models/Th11/CharaExReplacerTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th11;
+using ThScoreFileConverterTests.Models.Th10.Stubs;
+using IClearData = ThScoreFileConverter.Models.Th10.IClearData<
+    ThScoreFileConverter.Models.Th11.CharaWithTotal, ThScoreFileConverter.Models.Th11.StageProgress>;
+
+namespace ThScoreFileConverterTests.Models.Th11
+{
+    [TestClass]
+    public class CharaExReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuSuika,
+                    TotalPlayCount = 23,
+                    PlayTime = 4567890,
+                    ClearCounts = Utils.GetEnumerator<Level>().ToDictionary(level => level, level => 100 - (int)level),
+                },
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.MarisaAlice,
+                    TotalPlayCount = 12,
+                    PlayTime = 3456789,
+                    ClearCounts = Utils.GetEnumerator<Level>().ToDictionary(level => level, level => 50 - (int)level),
+                },
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void CharaExReplacerTest()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CharaExReplacerTestNull()
+        {
+            _ = new CharaExReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void CharaExReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CharaExReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalPlayCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("23", replacer.Replace("%T11CHARAEXHRS1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestPlayTime()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("21:08:51", replacer.Replace("%T11CHARAEXHRS2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestClearCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("98", replacer.Replace("%T11CHARAEXHRS3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotalTotalPlayCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("23", replacer.Replace("%T11CHARAEXTRS1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotalPlayTime()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("21:08:51", replacer.Replace("%T11CHARAEXTRS2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelTotalClearCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("490", replacer.Replace("%T11CHARAEXTRS3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotalTotalPlayCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("35", replacer.Replace("%T11CHARAEXHTL1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotalPlayTime()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("37:09:04", replacer.Replace("%T11CHARAEXHTL2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestCharaTotalClearCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("146", replacer.Replace("%T11CHARAEXHTL3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalTotalPlayCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("35", replacer.Replace("%T11CHARAEXTTL1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalPlayTime()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("37:09:04", replacer.Replace("%T11CHARAEXTTL2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestTotalClearCount()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("730", replacer.Replace("%T11CHARAEXTTL3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CharaExReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T11CHARAEXHRS1"));
+            Assert.AreEqual("0:00:00", replacer.Replace("%T11CHARAEXHRS2"));
+            Assert.AreEqual("0", replacer.Replace("%T11CHARAEXHRS3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyClearCounts()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>
+                {
+                    Chara = CharaWithTotal.ReimuSuika,
+                    ClearCounts = new Dictionary<Level, int>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new CharaExReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T11CHARAEXHRS3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T11XXXXXXXHRS1", replacer.Replace("%T11XXXXXXXHRS1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T11CHARAEXYRS1", replacer.Replace("%T11CHARAEXYRS1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T11CHARAEXHXX1", replacer.Replace("%T11CHARAEXHXX1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new CharaExReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T11CHARAEXHRSX", replacer.Replace("%T11CHARAEXHRSX"));
+        }
+    }
+}
