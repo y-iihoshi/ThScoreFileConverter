e51b1903e46e524c990e1c26a1f0cc4e350866d6
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-03 14:34:54 +0900

    Add test cases for Models.Th11.CardReplacer

---
+++ ThScoreFileConverterTests/Models/Th11/CardReplacerTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models.Th11;
+using ThScoreFileConverterTests.Models.Th10.Stubs;
+using IClearData = ThScoreFileConverter.Models.Th10.IClearData<
+    ThScoreFileConverter.Models.Th11.CharaWithTotal, ThScoreFileConverter.Models.Th11.StageProgress>;
+using ISpellCard = ThScoreFileConverter.Models.Th10.ISpellCard<ThScoreFileConverter.Models.Level>;
+
+namespace ThScoreFileConverterTests.Models.Th11
+{
+    [TestClass]
+    public class CardReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>()
+                {
+                    Chara = CharaWithTotal.Total,
+                    Cards = new Dictionary<int, ISpellCard>()
+                    {
+                        { 3, new SpellCardStub() { HasTried = true } },
+                        { 4, new SpellCardStub() { HasTried = false } },
+                    },
+                },
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void CardReplacerTest()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CardReplacerTestNull()
+        {
+            _ = new CardReplacer(null, false);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void CardReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CardReplacer(dictionary, false);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestName()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.AreEqual("罠符「キャプチャーウェブ」", replacer.Replace("%T11CARD003N"));
+            Assert.AreEqual("罠符「キャプチャーウェブ」", replacer.Replace("%T11CARD004N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestRank()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.AreEqual("Easy", replacer.Replace("%T11CARD003R"));
+            Assert.AreEqual("Normal", replacer.Replace("%T11CARD004R"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestHiddenName()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, true);
+            Assert.AreEqual("罠符「キャプチャーウェブ」", replacer.Replace("%T11CARD003N"));
+            Assert.AreEqual("??????????", replacer.Replace("%T11CARD004N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestHiddenRank()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, true);
+            Assert.AreEqual("Easy", replacer.Replace("%T11CARD003R"));
+            Assert.AreEqual("Normal", replacer.Replace("%T11CARD004R"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+
+            var replacer = new CardReplacer(dictionary, true);
+            Assert.AreEqual("??????????", replacer.Replace("%T11CARD003N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyCards()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub<CharaWithTotal, StageProgress>()
+                {
+                    Chara = CharaWithTotal.Total,
+                    Cards = new Dictionary<int, ISpellCard>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new CardReplacer(dictionary, true);
+            Assert.AreEqual("??????????", replacer.Replace("%T11CARD003N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.AreEqual("%T11XXXX003N", replacer.Replace("%T11XXXX003N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidNumber()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.AreEqual("%T11CARD176N", replacer.Replace("%T11CARD176N"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new CardReplacer(ClearDataDictionary, false);
+            Assert.AreEqual("%T11CARD003X", replacer.Replace("%T11CARD003X"));
+        }
+    }
+}
