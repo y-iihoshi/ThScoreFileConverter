17f620f82d105920977e06f5cf8d89f7298b6237
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-03 14:00:59 +0900

    Separate replacer classes from Models.Th11Converter

---
+++ ThScoreFileConverter/Models/Th11/CardReplacer.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CardReplacer.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#pragma warning disable SA1600 // Elements should be documented
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ThScoreFileConverter.Models.Th11
+{
+    // %T11CARD[xxx][y]
+    internal class CardReplacer : IStringReplaceable
+    {
+        private const string Pattern = @"%T11CARD(\d{3})([NR])";
+
+        private readonly MatchEvaluator evaluator;
+
+        public CardReplacer(
+            IReadOnlyDictionary<CharaWithTotal, Th10.IClearData<CharaWithTotal, StageProgress>> clearDataDictionary,
+            bool hideUntriedCards)
+        {
+            this.evaluator = new MatchEvaluator(match =>
+            {
+                var number = int.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                var type = match.Groups[2].Value.ToUpperInvariant();
+
+                if (Definitions.CardTable.ContainsKey(number))
+                {
+                    if (type == "N")
+                    {
+                        if (hideUntriedCards)
+                        {
+                            var cards = clearDataDictionary[CharaWithTotal.Total].Cards;
+                            if (!cards.TryGetValue(number, out var card) || !card.HasTried)
+                                return "??????????";
+                        }
+
+                        return Definitions.CardTable[number].Name;
+                    }
+                    else
+                    {
+                        return Definitions.CardTable[number].Level.ToString();
+                    }
+                }
+                else
+                {
+                    return match.ToString();
+                }
+            });
+        }
+
+        public string Replace(string input) => Regex.Replace(input, Pattern, this.evaluator, RegexOptions.IgnoreCase);
+    }
+}
---
+++ ThScoreFileConverter/Models/Th11/CollectRateReplacer.cs
@@ -0,0 +1,76 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CollectRateReplacer.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#pragma warning disable SA1600 // Elements should be documented
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ThScoreFileConverter.Models.Th11
+{
+    // %T11CRG[w][xx][y][z]
+    internal class CollectRateReplacer : IStringReplaceable
+    {
+        private static readonly string Pattern = Utils.Format(
+            @"%T11CRG({0})({1})({2})([12])",
+            Parsers.LevelWithTotalParser.Pattern,
+            Parsers.CharaWithTotalParser.Pattern,
+            Parsers.StageWithTotalParser.Pattern);
+
+        private readonly MatchEvaluator evaluator;
+
+        public CollectRateReplacer(
+            IReadOnlyDictionary<CharaWithTotal, Th10.IClearData<CharaWithTotal, StageProgress>> clearDataDictionary)
+        {
+            this.evaluator = new MatchEvaluator(match =>
+            {
+                var level = Parsers.LevelWithTotalParser.Parse(match.Groups[1].Value);
+                var chara = Parsers.CharaWithTotalParser.Parse(match.Groups[2].Value);
+                var stage = Parsers.StageWithTotalParser.Parse(match.Groups[3].Value);
+                var type = int.Parse(match.Groups[4].Value, CultureInfo.InvariantCulture);
+
+                if (stage == StageWithTotal.Extra)
+                    return match.ToString();
+
+                Func<Th10.ISpellCard<Level>, bool> findByStage;
+                if (stage == StageWithTotal.Total)
+                    findByStage = card => true;
+                else
+                    findByStage = card => Definitions.CardTable[card.Id].Stage == (Stage)stage;
+
+                Func<Th10.ISpellCard<Level>, bool> findByLevel = card => true;
+                switch (level)
+                {
+                    case LevelWithTotal.Total:
+                        // Do nothing
+                        break;
+                    case LevelWithTotal.Extra:
+                        findByStage = card => Definitions.CardTable[card.Id].Stage == Stage.Extra;
+                        break;
+                    default:
+                        findByLevel = card => card.Level == (Level)level;
+                        break;
+                }
+
+                Func<Th10.ISpellCard<Level>, bool> findByType;
+                if (type == 1)
+                    findByType = card => card.ClearCount > 0;
+                else
+                    findByType = card => card.TrialCount > 0;
+
+                return clearDataDictionary[chara].Cards.Values
+                    .Count(Utils.MakeAndPredicate(findByLevel, findByStage, findByType))
+                    .ToString(CultureInfo.CurrentCulture);
+            });
+        }
+
+        public string Replace(string input) => Regex.Replace(input, Pattern, this.evaluator, RegexOptions.IgnoreCase);
+    }
+}
