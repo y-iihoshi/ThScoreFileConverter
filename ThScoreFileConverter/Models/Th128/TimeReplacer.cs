73917bc8aa47032388b7e7aef304931e87cd65bc
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-09 14:49:48 +0900

    Add test cases for Models.Th128.TimeReplacer

--- ThScoreFileConverter/Models/Th128/TimeReplacer.cs
+++ ThScoreFileConverter/Models/Th128/TimeReplacer.cs
@@ -7,6 +7,7 @@
 
 #pragma warning disable SA1600 // Elements should be documented
 
+using System;
 using System.Text.RegularExpressions;
 
 namespace ThScoreFileConverter.Models.Th128
@@ -19,7 +20,12 @@ namespace ThScoreFileConverter.Models.Th128
         private readonly MatchEvaluator evaluator;
 
         public TimeReplacer(Th125.IStatus status)
-            => this.evaluator = new MatchEvaluator(match => new Time(status.TotalPlayTime * 10, false).ToLongString());
+        {
+            if (status is null)
+                throw new ArgumentNullException(nameof(status));
+
+            this.evaluator = new MatchEvaluator(match => new Time(status.TotalPlayTime * 10, false).ToLongString());
+        }
 
         public string Replace(string input) => Regex.Replace(input, Pattern, this.evaluator, RegexOptions.IgnoreCase);
     }
---
+++ ThScoreFileConverterTests/Models/Th128/TimeReplacerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models.Th128;
+
+namespace ThScoreFileConverterTests.Models.Th128
+{
+    [TestClass]
+    public class TimeReplacerTests
+    {
+        [TestMethod]
+        public void TimeReplacerTest()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TimeReplacerTestNull()
+        {
+            _ = new TimeReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.AreEqual("34:17:36.780", replacer.Replace("%T128TIMEPLY"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.AreEqual("%T128XXXXXXX", replacer.Replace("%T128XXXXXXX"));
+        }
+    }
+}
