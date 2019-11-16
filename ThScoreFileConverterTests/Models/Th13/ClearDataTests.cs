7629ebb4e6d4069ca13c2bb84432418c5781728f
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-16 22:29:55 +0900

    Add test cases for Models.Th13.ScoreReplacer

--- ThScoreFileConverterTests/Models/Th13/ClearDataTests.cs
+++ ThScoreFileConverterTests/Models/Th13/ClearDataTests.cs
@@ -50,7 +50,7 @@ namespace ThScoreFileConverterTests.Models.Th13
                             Score = 12345670u - (uint)index * 1000u,
                             StageProgress = StageProgress.Five,
                             ContinueCount = (byte)index,
-                            Name = TestUtils.MakeRandomArray<byte>(10),
+                            Name = TestUtils.CP932Encoding.GetBytes($"Player{index}\0\0\0"),
                             DateTime = 34567890u,
                             SlowRate = 1.2f
                         }).ToList() as IReadOnlyList<ThScoreFileConverter.Models.Th10.IScoreData<StageProgress>>),
---
+++ ThScoreFileConverterTests/Models/Th13/ScoreReplacerTests.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th13;
+using ThScoreFileConverterTests.Models.Th10.Stubs;
+using ClearDataStub = ThScoreFileConverterTests.Models.Th13.Stubs.ClearDataStub<
+    ThScoreFileConverter.Models.Th13.CharaWithTotal,
+    ThScoreFileConverter.Models.Th13.LevelPractice,
+    ThScoreFileConverter.Models.Th13.LevelPractice,
+    ThScoreFileConverter.Models.Th13.LevelPracticeWithTotal,
+    ThScoreFileConverter.Models.Th13.StagePractice>;
+using IClearData = ThScoreFileConverter.Models.Th13.IClearData<
+    ThScoreFileConverter.Models.Th13.CharaWithTotal,
+    ThScoreFileConverter.Models.Th13.LevelPractice,
+    ThScoreFileConverter.Models.Th13.LevelPractice,
+    ThScoreFileConverter.Models.Th13.LevelPracticeWithTotal,
+    ThScoreFileConverter.Models.Th13.StagePractice>;
+using IScoreData = ThScoreFileConverter.Models.Th10.IScoreData<ThScoreFileConverter.Models.Th13.StageProgress>;
+
+namespace ThScoreFileConverterTests.Models.Th13
+{
+    [TestClass]
+    public class ScoreReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                ClearDataTests.MakeValidStub(),
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void ScoreReplacerTest()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ScoreReplacerTestNull()
+        {
+            _ = new ScoreReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ScoreReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestName()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("Player1", replacer.Replace("%T13SCRHMR21"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestScore()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("123,446,701", replacer.Replace("%T13SCRHMR22"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("123446701", replacer.Replace("%T13SCRHMR22"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestStage()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("Stage 5", replacer.Replace("%T13SCRHMR23"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestDateTime()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            var expected = new DateTime(1970, 1, 1).AddSeconds(34567890).ToLocalTime().ToString("yyyy/MM/dd HH:mm:ss");
+            Assert.AreEqual(expected, replacer.Replace("%T13SCRHMR24"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestSlowRate()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("1.200%", replacer.Replace("%T13SCRHMR25"));  // really...?
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.AreEqual("--------", replacer.Replace("%T13SCRHMR21"));
+            Assert.AreEqual("0", replacer.Replace("%T13SCRHMR22"));
+            Assert.AreEqual("-------", replacer.Replace("%T13SCRHMR23"));
+            Assert.AreEqual("----/--/-- --:--:--", replacer.Replace("%T13SCRHMR24"));
+            Assert.AreEqual("-----%", replacer.Replace("%T13SCRHMR25"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyRankings()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub()
+                {
+                    Chara = CharaWithTotal.Marisa,
+                    Rankings = new Dictionary<LevelPracticeWithTotal, IReadOnlyList<IScoreData>>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.AreEqual("--------", replacer.Replace("%T13SCRHMR21"));
+            Assert.AreEqual("0", replacer.Replace("%T13SCRHMR22"));
+            Assert.AreEqual("-------", replacer.Replace("%T13SCRHMR23"));
+            Assert.AreEqual("----/--/-- --:--:--", replacer.Replace("%T13SCRHMR24"));
+            Assert.AreEqual("-----%", replacer.Replace("%T13SCRHMR25"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyRanking()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub()
+                {
+                    Chara = CharaWithTotal.Marisa,
+                    Rankings = Utils.GetEnumerator<LevelPracticeWithTotal>().ToDictionary(
+                        level => level,
+                        level => new List<IScoreData>() as IReadOnlyList<IScoreData>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.AreEqual("--------", replacer.Replace("%T13SCRHMR21"));
+            Assert.AreEqual("0", replacer.Replace("%T13SCRHMR22"));
+            Assert.AreEqual("-------", replacer.Replace("%T13SCRHMR23"));
+            Assert.AreEqual("----/--/-- --:--:--", replacer.Replace("%T13SCRHMR24"));
+            Assert.AreEqual("-----%", replacer.Replace("%T13SCRHMR25"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStageExtra()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub()
+                {
+                    Chara = CharaWithTotal.Marisa,
+                    Rankings = Utils.GetEnumerator<LevelPracticeWithTotal>().ToDictionary(
+                        level => level,
+                        level => Enumerable.Range(0, 10).Select(
+                            index => new ScoreDataStub<StageProgress>()
+                            {
+                                DateTime = 34567890u,
+                                StageProgress = StageProgress.Extra,
+                            }).ToList() as IReadOnlyList<IScoreData>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.AreEqual("Not Clear", replacer.Replace("%T13SCRHMR23"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStageExtraClear()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub()
+                {
+                    Chara = CharaWithTotal.Marisa,
+                    Rankings = Utils.GetEnumerator<LevelPracticeWithTotal>().ToDictionary(
+                        level => level,
+                        level => Enumerable.Range(0, 10).Select(
+                            index => new ScoreDataStub<StageProgress>()
+                            {
+                                DateTime = 34567890u,
+                                StageProgress = StageProgress.ExtraClear,
+                            }).ToList() as IReadOnlyList<IScoreData>),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new ScoreReplacer(dictionary);
+            Assert.AreEqual("All Clear", replacer.Replace("%T13SCRHMR23"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T13XXXHMR21", replacer.Replace("%T13XXXHMR21"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T13SCRYMR21", replacer.Replace("%T13SCRYMR21"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T13SCRHXX21", replacer.Replace("%T13SCRHXX21"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidRank()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T13SCRHMRX1", replacer.Replace("%T13SCRHMRX1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new ScoreReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T13SCRHMR2X", replacer.Replace("%T13SCRHMR2X"));
+        }
+    }
+}
