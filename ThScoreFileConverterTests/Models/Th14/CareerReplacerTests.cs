9b2755ddebdc7bcb30ae9356c438e836dd3de2fd
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-23 20:58:03 +0900

    Add test cases for Models.Th14.CareerReplacer

---
+++ ThScoreFileConverterTests/Models/Th14/CareerReplacerTests.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter;
+using ThScoreFileConverter.Models.Th14;
+using ClearDataStub = ThScoreFileConverterTests.Models.Th13.Stubs.ClearDataStub<
+    ThScoreFileConverter.Models.Th14.CharaWithTotal,
+    ThScoreFileConverter.Models.Level,
+    ThScoreFileConverter.Models.Th14.LevelPractice,
+    ThScoreFileConverter.Models.Th14.LevelPracticeWithTotal,
+    ThScoreFileConverter.Models.Th14.StagePractice>;
+using IClearData = ThScoreFileConverter.Models.Th13.IClearData<
+    ThScoreFileConverter.Models.Th14.CharaWithTotal,
+    ThScoreFileConverter.Models.Level,
+    ThScoreFileConverter.Models.Th14.LevelPractice,
+    ThScoreFileConverter.Models.Th14.LevelPracticeWithTotal,
+    ThScoreFileConverter.Models.Th14.StagePractice>;
+using ISpellCard = ThScoreFileConverter.Models.Th13.ISpellCard<ThScoreFileConverter.Models.Level>;
+
+namespace ThScoreFileConverterTests.Models.Th14
+{
+    [TestClass]
+    public class CareerReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                ClearDataTests.MakeValidStub(),
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void CareerReplacerTest()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CareerReplacerTestNull()
+        {
+            _ = new CareerReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void CareerReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CareerReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTestStoryClearCount()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("15", replacer.Replace("%T14CS003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStoryTrialCount()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("59", replacer.Replace("%T14CS003RB2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStoryTotalClearCount()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new CareerReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("8,700", replacer.Replace("%T14CS000RB1"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("8700", replacer.Replace("%T14CS000RB1"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestStoryTotalTrialCount()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new CareerReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("13,980", replacer.Replace("%T14CS000RB2"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("13980", replacer.Replace("%T14CS000RB2"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestPracticeClearCount()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("37", replacer.Replace("%T14CP003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestPracticeTrialCount()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("81", replacer.Replace("%T14CP003RB2"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestPracticeTotalClearCount()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new CareerReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("11,340", replacer.Replace("%T14CP000RB1"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("11340", replacer.Replace("%T14CP000RB1"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestPracticeTotalTrialCount()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new CareerReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("16,620", replacer.Replace("%T14CP000RB2"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("16620", replacer.Replace("%T14CP000RB2"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new CareerReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T14CS003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyCards()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub()
+                {
+                    Chara = CharaWithTotal.ReimuB,
+                    Cards = new Dictionary<int, ISpellCard>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new CareerReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T14CS003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T14XS003RB1", replacer.Replace("%T14XS003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidGameMode()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T14CX003RB1", replacer.Replace("%T14CX003RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidNumber()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T14CS121RB1", replacer.Replace("%T14CS121RB1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T14CS003XX1", replacer.Replace("%T14CS003XX1"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidType()
+        {
+            var replacer = new CareerReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T14CS003RBX", replacer.Replace("%T14CS003RBX"));
+        }
+    }
+}
