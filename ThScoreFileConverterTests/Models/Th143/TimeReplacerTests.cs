ca22098d25eedc9c08be0f199050d1146931b4de
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-24 19:28:24 +0900

    Add test cases for Models.Th143.TimeReplacer

---
+++ ThScoreFileConverterTests/Models/Th143/TimeReplacerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Models.Th143;
+
+namespace ThScoreFileConverterTests.Models.Th143
+{
+    [TestClass]
+    public class TimeReplacerTests
+    {
+        [TestMethod]
+        public void TimeReplacerTest()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TimeReplacerTestNull()
+        {
+            _ = new TimeReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.AreEqual("34:17:36.780", replacer.Replace("%T143TIMEPLY"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new TimeReplacer(StatusTests.ValidStub);
+            Assert.AreEqual("%T143XXXXXXX", replacer.Replace("%T143XXXXXXX"));
+        }
+    }
+}
