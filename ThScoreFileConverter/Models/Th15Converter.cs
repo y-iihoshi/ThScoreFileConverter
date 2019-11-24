cf3fe5080e48ae7999f0714b0f4737237d158178
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-25 00:12:28 +0900

    Move AllScoreData to Models.Th15

--- ThScoreFileConverter/Models/Th15Converter.cs
+++ ThScoreFileConverter/Models/Th15Converter.cs
@@ -620,32 +620,5 @@ namespace ThScoreFileConverter.Models
                 return Regex.Replace(input, Pattern, this.evaluator, RegexOptions.IgnoreCase);
             }
         }
-
-        private class AllScoreData
-        {
-            private readonly Dictionary<CharaWithTotal, IClearData> clearData;
-
-            public AllScoreData()
-            {
-                this.clearData =
-                    new Dictionary<CharaWithTotal, IClearData>(Enum.GetValues(typeof(CharaWithTotal)).Length);
-            }
-
-            public Th095.HeaderBase Header { get; private set; }
-
-            public IReadOnlyDictionary<CharaWithTotal, IClearData> ClearData => this.clearData;
-
-            public Th125.IStatus Status { get; private set; }
-
-            public void Set(Th095.HeaderBase header) => this.Header = header;
-
-            public void Set(IClearData data)
-            {
-                if (!this.clearData.ContainsKey(data.Chara))
-                    this.clearData.Add(data.Chara, data);
-            }
-
-            public void Set(Th125.IStatus status) => this.Status = status;
-        }
     }
 }
