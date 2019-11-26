bac0e906093a885e59ad997613600d6e69e52ea0
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-26 23:33:09 +0900

    Add test cases for Models.Th155.ClearRankReplacer

--- ThScoreFileConverter/Models/Th155/ClearRankReplacer.cs
+++ ThScoreFileConverter/Models/Th155/ClearRankReplacer.cs
@@ -7,6 +7,7 @@
 
 #pragma warning disable SA1600 // Elements should be documented
 
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -22,6 +23,9 @@ namespace ThScoreFileConverter.Models.Th155
 
         public ClearRankReplacer(IReadOnlyDictionary<StoryChara, AllScoreData.Story> storyDictionary)
         {
+            if (storyDictionary is null)
+                throw new ArgumentNullException(nameof(storyDictionary));
+
             this.evaluator = new MatchEvaluator(match =>
             {
                 var level = Parsers.LevelParser.Parse(match.Groups[1].Value);
