4d159a7c23fc5bac8e5ebbd9741ce7fe1fcd2aec
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-11-26 21:22:10 +0900

    Add test cases for Models.Th15.PracticeReplacer

---
+++ ThScoreFileConverterTests/Models/Th15/PracticeReplacerTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th15;
+using ThScoreFileConverterTests.Models.Th15.Stubs;
+using IPractice = ThScoreFileConverter.Models.Th13.IPractice;
+
+namespace ThScoreFileConverterTests.Models.Th15
+{
+    [TestClass]
+    public class PracticeReplacerTests
+    {
+        internal static IReadOnlyDictionary<CharaWithTotal, IClearData> ClearDataDictionary { get; } =
+            new List<IClearData>
+            {
+                ClearDataTests.MakeValidStub(),
+            }.ToDictionary(element => element.Chara);
+
+        [TestMethod]
+        public void PracticeReplacerTest()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PracticeReplacerTestNull()
+        {
+            _ = new PracticeReplacer(null);
+            Assert.Fail(TestUtils.Unreachable);
+        }
+
+        [TestMethod]
+        public void PracticeReplacerTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new PracticeReplacer(dictionary);
+            Assert.IsNotNull(replacer);
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            var outputSeparator = Settings.Instance.OutputNumberGroupSeparator;
+
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+
+            Settings.Instance.OutputNumberGroupSeparator = true;
+            Assert.AreEqual("1,234,360", replacer.Replace("%T15PRACHMR3"));
+
+            Settings.Instance.OutputNumberGroupSeparator = false;
+            Assert.AreEqual("1234360", replacer.Replace("%T15PRACHMR3"));
+
+            Settings.Instance.OutputNumberGroupSeparator = outputSeparator;
+        }
+
+        [TestMethod]
+        public void ReplaceTestLevelExtra()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15PRACXMR3", replacer.Replace("%T15PRACXMR3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestStageExtra()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15PRACHMRX", replacer.Replace("%T15PRACHMRX"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmpty()
+        {
+            var dictionary = new Dictionary<CharaWithTotal, IClearData>();
+            var replacer = new PracticeReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T15PRACHMR3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestEmptyPractices()
+        {
+            var dictionary = new List<IClearData>
+            {
+                new ClearDataStub
+                {
+                    Chara = CharaWithTotal.Marisa,
+                    Practices = new Dictionary<(Level, StagePractice), IPractice>(),
+                },
+            }.ToDictionary(element => element.Chara);
+
+            var replacer = new PracticeReplacer(dictionary);
+            Assert.AreEqual("0", replacer.Replace("%T15PRACHMR3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidFormat()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15XXXXHMR3", replacer.Replace("%T15XXXXHMR3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidLevel()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15PRACYMR3", replacer.Replace("%T15PRACYMR3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidChara()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15PRACHXX3", replacer.Replace("%T15PRACHXX3"));
+        }
+
+        [TestMethod]
+        public void ReplaceTestInvalidStage()
+        {
+            var replacer = new PracticeReplacer(ClearDataDictionary);
+            Assert.AreEqual("%T15PRACHMRY", replacer.Replace("%T15PRACHMRY"));
+        }
+    }
+}
