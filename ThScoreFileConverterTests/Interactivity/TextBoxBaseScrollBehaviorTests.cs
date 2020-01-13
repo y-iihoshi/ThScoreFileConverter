3eea9a475823805b2ebe2905f161db44aaad6129
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-01-13 22:43:56 +0900

    Add test cases for Behaviors.*

---
+++ ThScoreFileConverterTests/Interactivity/TextBoxBaseScrollBehaviorTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xaml.Behaviors;
+using ThScoreFileConverter.Behaviors;
+using ThScoreFileConverterTests.Models;
+
+namespace ThScoreFileConverterTests.Interactivity
+{
+    internal class Logger : INotifyPropertyChanged
+    {
+        private string log = string.Empty;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Log
+        {
+            get => this.log;
+            set
+            {
+                this.log = value;
+                this.OnPropertyChanged(nameof(this.Log));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+            => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    [TestClass]
+    public class TextBoxBaseScrollBehaviorTests
+    {
+        [TestMethod]
+        public void AutoScrollToEndTest()
+        {
+            var logger = new Logger();
+            var binding = new Binding(nameof(logger.Log))
+            {
+                NotifyOnTargetUpdated = true,
+                Source = logger,
+            };
+            var textbox = new TextBox();
+            var window = new Window
+            {
+                Content = textbox,
+            };
+            var behavior = new TextBoxBaseScrollBehavior
+            {
+                AutoScrollToEnd = true,
+            };
+            var behaviors = Interaction.GetBehaviors(textbox);
+
+            static void onLayoutUpdated(object sender, EventArgs eventArgs)
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(TextBoxBaseScrollBehavior).FullName}.OnTargetUpdated"));
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(TextBoxBase).FullName}.{nameof(TextBoxBase.ScrollToEnd)}"));
+            }
+
+            BindingOperations.SetBinding(textbox, TextBox.TextProperty, binding);
+            behaviors.Add(behavior);
+            textbox.LayoutUpdated += onLayoutUpdated;
+
+            logger.Log = "abc";
+
+            textbox.LayoutUpdated -= onLayoutUpdated;
+            _ = behaviors.Remove(behavior);
+            BindingOperations.ClearBinding(textbox, TextBox.TextProperty);
+        }
+
+        [TestMethod]
+        public void NotAutoScrollToEndTest()
+        {
+            var logger = new Logger();
+            var binding = new Binding(nameof(logger.Log))
+            {
+                NotifyOnTargetUpdated = true,
+                Source = logger,
+            };
+            var textbox = new TextBox();
+            var window = new Window
+            {
+                Content = textbox,
+            };
+            var behavior = new TextBoxBaseScrollBehavior
+            {
+                AutoScrollToEnd = false,
+            };
+            var behaviors = Interaction.GetBehaviors(textbox);
+
+            static void onLayoutUpdated(object sender, EventArgs eventArgs)
+            {
+                Assert.Fail(TestUtils.Unreachable);
+            }
+
+            BindingOperations.SetBinding(textbox, TextBox.TextProperty, binding);
+            behaviors.Add(behavior);
+            textbox.LayoutUpdated += onLayoutUpdated;
+
+            logger.Log = "abc";
+
+            textbox.LayoutUpdated -= onLayoutUpdated;
+            _ = behaviors.Remove(behavior);
+            BindingOperations.ClearBinding(textbox, TextBox.TextProperty);
+        }
+    }
+}
---
+++ ThScoreFileConverterTests/Interactivity/UIElementDropBehaviorTests.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xaml.Behaviors;
+using ThScoreFileConverter.Behaviors;
+using ThScoreFileConverterTests.Models;
+
+namespace ThScoreFileConverterTests.Interactivity
+{
+    internal class Command : ICommand
+    {
+        private readonly Action action;
+
+        public Command(Action action)
+        {
+            this.action = action;
+        }
+
+#pragma warning disable CS0067
+        public event EventHandler? CanExecuteChanged;
+#pragma warning restore CS0067
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            Assert.IsTrue(parameter is DragEventArgs);
+            this.action.Invoke();
+        }
+    }
+
+    [TestClass]
+    public class UIElementDropBehaviorTests
+    {
+        private static DragEventArgs CreateDragEventArgs(DependencyObject target, RoutedEvent routedEvent)
+        {
+            var types = new[]
+            {
+                typeof(IDataObject),
+                typeof(DragDropKeyStates),
+                typeof(DragDropEffects),
+                typeof(DependencyObject),
+                typeof(Point),
+            };
+            var parameters = new object[]
+            {
+                new DataObject(),
+                DragDropKeyStates.None,
+                DragDropEffects.None,
+                target,
+                default(Point),
+            };
+
+            var dragEventArgs = (DragEventArgs)typeof(DragEventArgs)
+                .GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, types, null).Invoke(parameters);
+            dragEventArgs.RoutedEvent = routedEvent;
+
+            return dragEventArgs;
+        }
+
+        [TestMethod]
+        public void DragEnterTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnDragEnter"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                DragEnterCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.DragEnterEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void DragLeaveTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnDragLeave"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                DragLeaveCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.DragLeaveEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void DragOverTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnDragOver"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                DragOverCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.DragOverEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void DropTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnDrop"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                DropCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.DropEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void PreviewDragEnterTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnPreviewDragEnter"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                PreviewDragEnterCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.PreviewDragEnterEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void PreviewDragLeaveTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnPreviewDragLeave"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                PreviewDragLeaveCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.PreviewDragLeaveEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void PreviewDragOverTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnPreviewDragOver"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                PreviewDragOverCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.PreviewDragOverEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void PreviewDropTest()
+        {
+            var element = new UIElement();
+            var command = new Command(() =>
+            {
+                Assert.IsTrue(Environment.StackTrace.Contains(
+                    $"{typeof(UIElementDropBehavior).FullName}.OnPreviewDrop"));
+            });
+            var behavior = new UIElementDropBehavior
+            {
+                PreviewDropCommand = command,
+            };
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.PreviewDropEvent));
+
+            _ = behaviors.Remove(behavior);
+        }
+
+        [TestMethod]
+        public void NullCommandTest()
+        {
+            var element = new UIElement();
+            var behavior = new UIElementDropBehavior();
+            var behaviors = Interaction.GetBehaviors(element);
+
+            behaviors.Add(behavior);
+
+            element.RaiseEvent(CreateDragEventArgs(element, UIElement.DragEnterEvent));
+            Assert.IsTrue(true);  // Should be reached here.
+
+            _ = behaviors.Remove(behavior);
+        }
+    }
+}
