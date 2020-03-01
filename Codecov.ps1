9d939f600f68c4ce61ea14fa4323490506c51191
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-03-01 13:05:21 +0900

    Remove no longer used files

--- Codecov.ps1
+++
@@ -1,5 +0,0 @@
-[string]$inputFile = $args[0]
-
-$codecov = (Resolve-Path "$env:USERPROFILE\.nuget\packages\codecov\*\tools\codecov.exe").ToString()
-
-& $codecov -f $inputFile -t $env:CODECOV_TOKEN
