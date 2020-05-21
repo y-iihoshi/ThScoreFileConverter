f6872fdb78214f062df2833e30fd3ea63307eccd
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-05-21 22:38:27 +0900

    Fix to use pattern matching

--- ThScoreFileConverterTests/Models/Th165/HashtagFieldsTests.cs
+++ ThScoreFileConverterTests/Models/Th165/HashtagFieldsTests.cs
@@ -605,7 +605,7 @@ namespace ThScoreFileConverterTests.Models.Th165
             foreach (var prop in fields.GetType().GetProperties().Where(prop => prop.PropertyType == typeof(bool)))
             {
                 var value = prop.GetValue(fields);
-                Assert.IsTrue((value is bool) && !(bool)value);
+                Assert.IsTrue((value is bool boolean) && !boolean);
             }
         }
     }
