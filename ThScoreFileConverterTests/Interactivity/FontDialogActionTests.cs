c3c2330a7b27a51c59c144f612d51bbf38c0fc86
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-10-31 21:33:16 +0900

    Add tests for Interactivity.*DialogAction

---
+++ ThScoreFileConverterTests/Interactivity/FontDialogActionTests.cs
@@ -0,0 +1,174 @@
+﻿using System.Windows.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Interactivity;
+using SysDraw = System.Drawing;
+
+namespace ThScoreFileConverterTests.Interactivity
+{
+    [TestClass]
+    public class FontDialogActionTests
+    {
+        [TestMethod]
+        public void ApplyCommandTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsNull(action.ApplyCommand);
+
+            var command = ApplicationCommands.NotACommand;
+            action.ApplyCommand = command;
+            Assert.AreSame(command, action.ApplyCommand);
+        }
+
+        [TestMethod]
+        public void AllowScriptChangeTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsTrue(action.AllowScriptChange);
+
+            action.AllowScriptChange = false;
+            Assert.IsFalse(action.AllowScriptChange);
+        }
+
+        [TestMethod]
+        public void AllowSimulationsTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsTrue(action.AllowSimulations);
+
+            action.AllowSimulations = false;
+            Assert.IsFalse(action.AllowSimulations);
+        }
+
+        [TestMethod]
+        public void AllowVectorFontsTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsTrue(action.AllowVectorFonts);
+
+            action.AllowVectorFonts = false;
+            Assert.IsFalse(action.AllowVectorFonts);
+        }
+
+        [TestMethod]
+        public void AllowVerticalFontsTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsTrue(action.AllowVerticalFonts);
+
+            action.AllowVerticalFonts = false;
+            Assert.IsFalse(action.AllowVerticalFonts);
+        }
+
+        [TestMethod]
+        public void ColorTest()
+        {
+            var action = new FontDialogAction();
+            Assert.AreEqual(SysDraw.Color.Black, action.Color);
+
+            var color = SysDraw.Color.White;
+            action.Color = color;
+            Assert.AreEqual(color, action.Color);
+        }
+
+        [TestMethod]
+        public void FixedPitchOnlyTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.FixedPitchOnly);
+
+            action.FixedPitchOnly = true;
+            Assert.IsTrue(action.FixedPitchOnly);
+        }
+
+        [TestMethod]
+        public void FontTest()
+        {
+            var action = new FontDialogAction();
+            Assert.AreEqual(SysDraw.SystemFonts.DefaultFont, action.Font);
+
+            var font = SysDraw.SystemFonts.CaptionFont;
+            action.Font = font;
+            Assert.AreSame(font, action.Font);
+        }
+
+        [TestMethod]
+        public void FontMustExistTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.FontMustExist);
+
+            action.FontMustExist = true;
+            Assert.IsTrue(action.FontMustExist);
+        }
+
+        [TestMethod]
+        public void MaxSizeTest()
+        {
+            var action = new FontDialogAction();
+            Assert.AreEqual(0, action.MaxSize);
+
+            ++action.MaxSize;
+            Assert.AreEqual(1, action.MaxSize);
+        }
+
+        [TestMethod]
+        public void MinSizeTest()
+        {
+            var action = new FontDialogAction();
+            Assert.AreEqual(0, action.MinSize);
+
+            ++action.MinSize;
+            Assert.AreEqual(1, action.MinSize);
+        }
+
+        [TestMethod]
+        public void ScriptsOnlyTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.ScriptsOnly);
+
+            action.ScriptsOnly = true;
+            Assert.IsTrue(action.ScriptsOnly);
+        }
+
+        [TestMethod]
+        public void ShowApplyTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.ShowApply);
+
+            action.ShowApply = true;
+            Assert.IsTrue(action.ShowApply);
+        }
+
+        [TestMethod]
+        public void ShowColorTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.ShowColor);
+
+            action.ShowColor = true;
+            Assert.IsTrue(action.ShowColor);
+        }
+
+        [TestMethod]
+        public void ShowEffectsTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsTrue(action.ShowEffects);
+
+            action.ShowEffects = false;
+            Assert.IsFalse(action.ShowEffects);
+        }
+
+        [TestMethod]
+        public void ShowHelpTest()
+        {
+            var action = new FontDialogAction();
+            Assert.IsFalse(action.ShowHelp);
+
+            action.ShowHelp = true;
+            Assert.IsTrue(action.ShowHelp);
+        }
+    }
+}
---
+++ ThScoreFileConverterTests/Interactivity/OpenFileDialogActionTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Interactivity;
+
+namespace ThScoreFileConverterTests.Interactivity
+{
+    [TestClass]
+    public class OpenFileDialogActionTests
+    {
+        [TestMethod]
+        public void AddExtensionTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.AddExtension);
+
+            action.AddExtension = false;
+            Assert.IsFalse(action.AddExtension);
+        }
+
+        [TestMethod]
+        public void AutoUpgradeEnabledTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.AutoUpgradeEnabled);
+
+            action.AutoUpgradeEnabled = false;
+            Assert.IsFalse(action.AutoUpgradeEnabled);
+        }
+
+        [TestMethod]
+        public void CheckFileExistsTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.CheckFileExists);
+
+            action.CheckFileExists = false;
+            Assert.IsFalse(action.CheckFileExists);
+        }
+
+        [TestMethod]
+        public void CheckPathExistsTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.CheckPathExists);
+
+            action.CheckPathExists = false;
+            Assert.IsFalse(action.CheckPathExists);
+        }
+
+        [TestMethod]
+        public void DefaultExtTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(string.Empty, action.DefaultExt);
+
+            var extension = "dat";
+            action.DefaultExt = extension;
+            Assert.AreEqual(extension, action.DefaultExt);
+        }
+
+        [TestMethod]
+        public void DereferenceLinksTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.DereferenceLinks);
+
+            action.DereferenceLinks = false;
+            Assert.IsFalse(action.DereferenceLinks);
+        }
+
+        [TestMethod]
+        public void FileNameTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(string.Empty, action.FileName);
+
+            var name = "score.dat";
+            action.FileName = name;
+            Assert.AreEqual(name, action.FileName);
+        }
+
+        [TestMethod]
+        public void FilterTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(string.Empty, action.Filter);
+
+            var filter = "*.dat";
+            action.Filter = filter;
+            Assert.AreEqual(filter, action.Filter);
+        }
+
+        [TestMethod]
+        public void FilterIndexTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(1, action.FilterIndex);
+
+            ++action.FilterIndex;
+            Assert.AreEqual(2, action.FilterIndex);
+        }
+
+        [TestMethod]
+        public void InitialDirectoryTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(string.Empty, action.InitialDirectory);
+
+            var directory = Environment.CurrentDirectory;
+            action.InitialDirectory = directory;
+            Assert.AreEqual(directory, action.InitialDirectory);
+        }
+
+        [TestMethod]
+        public void MultiselectTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.Multiselect);
+
+            action.Multiselect = true;
+            Assert.IsTrue(action.Multiselect);
+        }
+
+        [TestMethod]
+        public void ReadOnlyCheckedTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.ReadOnlyChecked);
+
+            action.ReadOnlyChecked = true;
+            Assert.IsTrue(action.ReadOnlyChecked);
+        }
+
+        [TestMethod]
+        public void RestoreDirectoryTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.RestoreDirectory);
+
+            action.RestoreDirectory = true;
+            Assert.IsTrue(action.RestoreDirectory);
+        }
+
+        [TestMethod]
+        public void ShowHelpTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.ShowHelp);
+
+            action.ShowHelp = true;
+            Assert.IsTrue(action.ShowHelp);
+        }
+
+        [TestMethod]
+        public void ShowReadOnlyTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.ShowReadOnly);
+
+            action.ShowReadOnly = true;
+            Assert.IsTrue(action.ShowReadOnly);
+        }
+
+        [TestMethod]
+        public void SupportMultiDottedExtensionsTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsFalse(action.SupportMultiDottedExtensions);
+
+            action.SupportMultiDottedExtensions = true;
+            Assert.IsTrue(action.SupportMultiDottedExtensions);
+        }
+
+        [TestMethod]
+        public void TitleTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.AreEqual(string.Empty, action.Title);
+
+            var title = nameof(TitleTest);
+            action.Title = title;
+            Assert.AreEqual(title, action.Title);
+        }
+
+        [TestMethod]
+        public void ValidateNamesTest()
+        {
+            var action = new OpenFileDialogAction();
+            Assert.IsTrue(action.ValidateNames);
+
+            action.ValidateNames = false;
+            Assert.IsFalse(action.ValidateNames);
+        }
+    }
+}
