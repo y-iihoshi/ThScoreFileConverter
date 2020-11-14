a06f12dae04d2002e81a25f8481dfb531f9de782
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-11-14 14:50:30 +0900

    Fix to use Assert.ThrowsException<T>() in Models.Th135.*

--- ThScoreFileConverterTests/Models/Th135/AllScoreDataTests.cs
+++ ThScoreFileConverterTests/Models/Th135/AllScoreDataTests.cs
@@ -104,22 +104,17 @@ namespace ThScoreFileConverterTests.Models.Th135
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
         public void ReadFromTestNull()
         {
             var allScoreData = new AllScoreData();
-            allScoreData.ReadFrom(null!);
-
-            Assert.Fail(TestUtils.Unreachable);
+            _ = Assert.ThrowsException<ArgumentNullException>(() => allScoreData.ReadFrom(null!));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(EndOfStreamException))]
         public void ReadFromTestEmpty()
         {
-            _ = TestUtils.Create<AllScoreData>(Array.Empty<byte>());
-
-            Assert.Fail(TestUtils.Unreachable);
+            _ = Assert.ThrowsException<EndOfStreamException>(
+                () => _ = TestUtils.Create<AllScoreData>(Array.Empty<byte>()));
         }
 
         [TestMethod]
--- ThScoreFileConverterTests/Models/Th135/ClearReplacerTests.cs
+++ ThScoreFileConverterTests/Models/Th135/ClearReplacerTests.cs
@@ -22,12 +22,8 @@ namespace ThScoreFileConverterTests.Models.Th135
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
         public void ClearReplacerTestNull()
-        {
-            _ = new ClearReplacer(null!);
-            Assert.Fail(TestUtils.Unreachable);
-        }
+            => _ = Assert.ThrowsException<ArgumentNullException>(() => _ = new ClearReplacer(null!));
 
         [TestMethod]
         public void ClearReplacerTestEmpty()
