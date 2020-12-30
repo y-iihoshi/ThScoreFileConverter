06a56557ae711882770b1e48ec6867838a77d63b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-12-30 20:51:32 +0900

    Add Models.NumberFormatter

---
+++ ThScoreFileConverter/Models/INumberFormatter.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="INumberFormatter.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ThScoreFileConverter.Models
+{
+    /// <summary>
+    /// Defines several methods for formatting numbers.
+    /// </summary>
+    public interface INumberFormatter
+    {
+        /// <summary>
+        /// Converts the specified number to its equivalent string representation.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="number"/>.</typeparam>
+        /// <param name="number">A value to be converted.</param>
+        /// <returns>The string representation of <paramref name="number"/>.</returns>
+        string FormatNumber<T>(T number)
+            where T : struct;
+
+        /// <summary>
+        /// Converts a number to the string that represents a percentage.
+        /// </summary>
+        /// <param name="number">A value to be converted to a string. It should have been multiplied by 100.</param>
+        /// <param name="precision">The number of fractional digits of the percentage.</param>
+        /// <returns>The string representation of a percentage.</returns>
+        string FormatPercent(double number, int precision);
+    }
+}
---
+++ ThScoreFileConverter/Models/NumberFormatter.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NumberFormatter.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace ThScoreFileConverter.Models
+{
+    /// <summary>
+    /// Provides several methods for formatting numbers.
+    /// </summary>
+    internal class NumberFormatter : INumberFormatter
+    {
+        private readonly ISettings settings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NumberFormatter"/> class.
+        /// </summary>
+        /// <param name="settings">The settings of this application.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="settings"/> is <c>null</c>.</exception>
+        public NumberFormatter(ISettings settings)
+        {
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+
+        /// <inheritdoc/>
+        public string FormatNumber<T>(T number)
+            where T : struct
+        {
+            return (this.settings.OutputNumberGroupSeparator is bool output && output)
+                ? Utils.Format("{0:N0}", number) : (number.ToString() ?? string.Empty);
+        }
+
+        /// <inheritdoc/>
+        public string FormatPercent(double number, int precision)
+        {
+            if (precision is < 0 or > 99)
+                throw new ArgumentOutOfRangeException(nameof(precision));
+
+            return Utils.Format($"{{0:F{precision}}}%", number);
+        }
+    }
+}
