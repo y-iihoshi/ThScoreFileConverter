2ac158b77b2184816fe850a413ddc1095a9f9692
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-12-30 22:36:14 +0900

    Revise tests for Models.ThConverter

--- ThScoreFileConverterTests/Models/ThConverterTests.cs
+++ ThScoreFileConverterTests/Models/ThConverterTests.cs
@@ -52,8 +52,10 @@ namespace ThScoreFileConverterTests.Models
         }
 
         [TestMethod]
-        public void ConvertTestNoSettings()
+        public void ConvertTestNullSettings()
         {
+            var settings = (SettingsPerTitle)null!;
+            var formatter = new Mock<INumberFormatter>().Object;
             var converter = new Mock<ThConverter> { CallBase = true }.Object;
 
             converter.ConvertFinished +=
@@ -64,7 +66,43 @@ namespace ThScoreFileConverterTests.Models
                 (sender, e) => Console.WriteLine(
                     Utils.Format("{0}: {1}", nameof(converter.ExceptionOccurred), e.Exception.ToString()));
 
-            _ = Assert.ThrowsException<ArgumentException>(() => converter.Convert(new SettingsPerTitle()));
+            _ = Assert.ThrowsException<ArgumentException>(() => converter.Convert((settings, formatter)));
+        }
+
+        [TestMethod]
+        public void ConvertTestNullNumberFormatter()
+        {
+            var settings = new SettingsPerTitle();
+            var formatter = (INumberFormatter)null!;
+            var converter = new Mock<ThConverter> { CallBase = true }.Object;
+
+            converter.ConvertFinished +=
+                (sender, e) => Assert.Fail(nameof(converter.ConvertFinished) + ": " + TestUtils.Unreachable);
+            converter.ConvertAllFinished +=
+                (sender, e) => Assert.Fail(nameof(converter.ConvertAllFinished) + ": " + TestUtils.Unreachable);
+            converter.ExceptionOccurred +=
+                (sender, e) => Console.WriteLine(
+                    Utils.Format("{0}: {1}", nameof(converter.ExceptionOccurred), e.Exception.ToString()));
+
+            _ = Assert.ThrowsException<ArgumentException>(() => converter.Convert((settings, formatter)));
+        }
+
+        [TestMethod]
+        public void ConvertTestNoSettings()
+        {
+            var settings = new SettingsPerTitle();
+            var formatter = new Mock<INumberFormatter>().Object;
+            var converter = new Mock<ThConverter> { CallBase = true }.Object;
+
+            converter.ConvertFinished +=
+                (sender, e) => Assert.Fail(nameof(converter.ConvertFinished) + ": " + TestUtils.Unreachable);
+            converter.ConvertAllFinished +=
+                (sender, e) => Assert.Fail(nameof(converter.ConvertAllFinished) + ": " + TestUtils.Unreachable);
+            converter.ExceptionOccurred +=
+                (sender, e) => Console.WriteLine(
+                    Utils.Format("{0}: {1}", nameof(converter.ExceptionOccurred), e.Exception.ToString()));
+
+            _ = Assert.ThrowsException<ArgumentException>(() => converter.Convert((settings, formatter)));
         }
     }
 }
