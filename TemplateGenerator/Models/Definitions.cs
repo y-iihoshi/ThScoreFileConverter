d98aec44d22336de0c1888ad5e07ae48045a8490
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2021-04-24 01:39:02 +0900

    Simplify T4 templates for TH06

---
+++ TemplateGenerator/Models/Definitions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ThScoreFileConverter.Extensions;
+using ThScoreFileConverter.Helpers;
+using ThScoreFileConverter.Models;
+
+namespace TemplateGenerator.Models
+{
+    public class Definitions
+    {
+        public static IReadOnlyDictionary<string, string> LevelNames { get; } = EnumHelper<Level>.Enumerable
+            .ToDictionary(level => level.ToShortName(), level => level.ToString());
+
+        public static IReadOnlyDictionary<string, string> LevelPracticeNames { get; } = EnumHelper<Level>.Enumerable
+            .Where(level => level != Level.Extra)
+            .ToDictionary(level => level.ToShortName(), level => level.ToString());
+
+        public static IReadOnlyDictionary<string, string> StageNames { get; } = new[]
+        {
+            (Stage.One,   "Stage 1"),
+            (Stage.Two,   "Stage 2"),
+            (Stage.Three, "Stage 3"),
+            (Stage.Four,  "Stage 4"),
+            (Stage.Five,  "Stage 5"),
+            (Stage.Six,   "Stage 6"),
+            (Stage.Extra, "Extra"),
+        }.ToDictionary(pair => pair.Item1.ToShortName(), pair => pair.Item2);
+
+        public static IReadOnlyDictionary<string, string> StagePracticeNames { get; } =
+            StageNames.Where(pair => pair.Key != Stage.Extra.ToShortName()).ToDictionary();
+
+        public static IReadOnlyDictionary<string, string> StageWithTotalNames { get; } = new[]
+        {
+            (StageWithTotal.One,   "Stage 1"),
+            (StageWithTotal.Two,   "Stage 2"),
+            (StageWithTotal.Three, "Stage 3"),
+            (StageWithTotal.Four,  "Stage 4"),
+            (StageWithTotal.Five,  "Stage 5"),
+            (StageWithTotal.Six,   "Stage 6"),
+            (StageWithTotal.Extra, "Extra"),
+            (StageWithTotal.Total, "Total"),
+        }.ToDictionary(pair => pair.Item1.ToShortName(), pair => pair.Item2);
+
+        public static IEnumerable<string> StageKeysTotalFirst { get; } =
+            StageWithTotalNames.Keys.TakeLast(1).Concat(StageWithTotalNames.Keys.SkipLast(1));
+
+        public static IEnumerable<string> StageKeysTotalLast { get; } =
+            StageWithTotalNames.Keys;
+    }
+}
---
+++ TemplateGenerator/Models/Th06/Definitions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ThScoreFileConverter.Extensions;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Models.Th06;
+
+namespace TemplateGenerator.Models.Th06
+{
+    public class Definitions : Models.Definitions
+    {
+        public static string Title { get; } = "東方紅魔郷";
+
+        public static IReadOnlyDictionary<string, string> CharacterNames { get; } = new[]
+        {
+            (Chara.ReimuA,  "博麗 霊夢（霊）"),
+            (Chara.ReimuB,  "博麗 霊夢（夢）"),
+            (Chara.MarisaA, "霧雨 魔理沙（魔）"),
+            (Chara.MarisaB, "霧雨 魔理沙（恋）"),
+        }.ToDictionary(pair => pair.Item1.ToShortName(), pair => pair.Item2);
+
+        public static IReadOnlyDictionary<string, int> NumCardsPerStage { get; } = new[]
+        {
+            (Stage.One,    3),
+            (Stage.Two,    4),
+            (Stage.Three,  7),
+            (Stage.Four,  18),
+            (Stage.Five,   8),
+            (Stage.Six,   11),
+            (Stage.Extra, 13),
+        }.ToDictionary(pair => pair.Item1.ToShortName(), pair => pair.Item2);
+
+        public static bool CanPractice(string levelKey, string stageKey)
+        {
+            return !(levelKey == Level.Easy.ToShortName() && stageKey == Stage.Six.ToShortName());
+        }
+    }
+}
