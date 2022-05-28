5ec71cce850ccc939eb1fbdda4c5a2ee1a0f4ff4
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2022-05-29 00:22:21 +0900

    Fix to avoid StyleCopAnalyzer warnings

--- ThScoreFileConverter.Core/AssemblyInfo.cs
+++ ThScoreFileConverter.Core/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
