ffebc18949bf38ba1e93876d4c30344b27fc522e
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2022-05-29 16:26:02 +0900

    Add comments

--- ThScoreFileConverter.Core/Models/Th075/SpellCardInfo.cs
+++ ThScoreFileConverter.Core/Models/Th075/SpellCardInfo.cs
@@ -5,12 +5,19 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-#pragma warning disable SA1600 // Elements should be documented
-
 namespace ThScoreFileConverter.Core.Models.Th075;
 
+/// <summary>
+/// Indicates information of a IMP spell card.
+/// </summary>
 public class SpellCardInfo
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SpellCardInfo"/> class.
+    /// </summary>
+    /// <param name="name">A name of the spell card.</param>
+    /// <param name="enemy">The enemy character who use the spell card.</param>
+    /// <param name="level">The level which the spell card is used.</param>
     public SpellCardInfo(string name, Chara enemy, Level level)
     {
         this.Name = name;
@@ -18,9 +25,18 @@ public class SpellCardInfo
         this.Level = level;
     }
 
+    /// <summary>
+    /// Gets a name of the current spell card.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// Gets the enemy character who uses the current spell card.
+    /// </summary>
     public Chara Enemy { get; }
 
+    /// <summary>
+    /// Gets the level which the current spell card is used.
+    /// </summary>
     public Level Level { get; }
 }
