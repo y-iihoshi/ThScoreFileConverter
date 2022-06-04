a800258adebcd16fb53eb49192cb6e5b402d12cc
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2022-06-04 14:22:32 +0900

    Add comments

--- ThScoreFileConverter.Core/Models/Th105/StageInfo.cs
+++ ThScoreFileConverter.Core/Models/Th105/StageInfo.cs
@@ -5,17 +5,25 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-#pragma warning disable SA1600 // Elements should be documented
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ThScoreFileConverter.Core.Models.Th105;
 
+/// <summary>
+/// Indicates information of a SWR stage.
+/// </summary>
+/// <typeparam name="TChara">The type of a enemy character.</typeparam>
 public class StageInfo<TChara>
     where TChara : struct, Enum
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StageInfo{TChara}"/> class.
+    /// </summary>
+    /// <param name="stage">The stage.</param>
+    /// <param name="enemy">The enemy character of <paramref name="stage"/>.</param>
+    /// <param name="cardIds">The identifiers of cards used by <paramref name="enemy"/>.</param>
     public StageInfo(Stage stage, TChara enemy, IEnumerable<int> cardIds)
     {
         this.Stage = stage;
@@ -23,9 +31,19 @@ public class StageInfo<TChara>
         this.CardIds = cardIds.ToList();
     }
 
+    /// <summary>
+    /// Gets the stage.
+    /// </summary>
     public Stage Stage { get; }
 
+    /// <summary>
+    /// Gets the enemy character of the current stage.
+    /// </summary>
     public TChara Enemy { get; }
 
-    public IEnumerable<int> CardIds { get; }    // 0-based
+    /// <summary>
+    /// Gets the identifiers of cards used by <see cref="Enemy"/>.
+    /// An identifier is a 0-based number.
+    /// </summary>
+    public IEnumerable<int> CardIds { get; }
 }
