97275493ffd909f6e87288e295f9adbb426d56de
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2022-09-25 16:39:37 +0900

    Add Models.ReadOnlyCP932String

---
+++ ThScoreFileConverter.Tests/Models/ReadOnlyCP932StringTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThScoreFileConverter.Core.Tests.UnitTesting;
+using ThScoreFileConverter.Models;
+using ThScoreFileConverter.Tests.UnitTesting;
+
+namespace ThScoreFileConverter.Tests.Models;
+
+[TestClass]
+public class ReadOnlyCP932StringTests
+{
+    [TestMethod]
+    public void ReadOnlyCP932StringTest()
+    {
+        var expectedBytes = TestUtils.CP932Encoding.GetBytes("博麗 霊夢\0霧雨 魔理沙");
+        var actual = new ReadOnlyCP932String(expectedBytes);
+
+        Assert.AreEqual("博麗 霊夢", actual.ToString());
+        Assert.AreNotEqual("博麗 霊夢\0霧雨 魔理沙", actual.ToString());
+        CollectionAssert.That.AreEqual(expectedBytes, actual.Bytes);
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+        var empty = ReadOnlyCP932String.Empty;
+        Assert.IsFalse(empty.Bytes.Any());
+        Assert.AreEqual(string.Empty, empty.ToString());
+    }
+
+    [TestMethod]
+    public void EmptyTestSameInstances()
+    {
+        var empty = ReadOnlyCP932String.Empty;
+        var empty2 = ReadOnlyCP932String.Empty;
+        Assert.AreSame(empty, empty2);
+    }
+}
---
+++ ThScoreFileConverter/Models/ReadOnlyCP932String.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ReadOnlyCP932String.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#pragma warning disable SA1600 // Elements should be documented
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThScoreFileConverter.Helpers;
+
+namespace ThScoreFileConverter.Models;
+
+internal class ReadOnlyCP932String
+{
+    private readonly byte[] bytes;
+    private readonly string str;
+
+    public ReadOnlyCP932String(IEnumerable<byte> bytes)
+    {
+        this.bytes = bytes.ToArray();
+        this.str = EncodingHelper.CP932.GetString(this.bytes).Split('\0')[0];
+    }
+
+    public static ReadOnlyCP932String Empty { get; } = new ReadOnlyCP932String(Array.Empty<byte>());
+
+    public IEnumerable<byte> Bytes => this.bytes;
+
+    public override string ToString()
+    {
+        return this.str;
+    }
+}
