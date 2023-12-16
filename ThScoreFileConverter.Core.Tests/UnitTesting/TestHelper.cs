a38985a6431f49a0473f0ebe88ab13e5512082c9
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2023-12-16 22:39:13 +0900

    Fix to avoid CA2021

--- ThScoreFileConverter.Core.Tests/UnitTesting/TestHelper.cs
+++ ThScoreFileConverter.Core.Tests/UnitTesting/TestHelper.cs
@@ -9,11 +9,7 @@ public static class TestHelper
     public static IEnumerable<object[]> GetInvalidEnumerators<TEnum>()
         where TEnum : struct, Enum
     {
-#if NETFRAMEWORK
         var values = Enum.GetValues(typeof(TEnum)).Cast<int>().ToArray();
-#else
-        var values = Enum.GetValues<TEnum>().Cast<int>().ToArray();
-#endif
         yield return new object[] { values.Min() - 1 };
         yield return new object[] { values.Max() + 1 };
     }
