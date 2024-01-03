8904d453c3822596efff84e30c1d6eb6383ecaa1
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2024-01-03 16:29:04 +0900

    Start to use Statiq.Framework

---
+++ TemplateGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿#if NETFRAMEWORK
+
+return;
+
+#else
+
+#pragma warning disable CA2007 // Do not directly await a Task
+
+using Statiq.App;
+
+return await Bootstrapper
+    .Factory
+    .CreateDefault(args)
+    .RunAsync();
+
+#endif
