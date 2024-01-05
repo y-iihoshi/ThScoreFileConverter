85eccb90e0b9f397ee2caa6f1517da7144f2ab86
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2024-01-05 20:29:03 +0900

    Start to use Statiq.Razor

--- TemplateGenerator/Program.cs
+++ TemplateGenerator/Program.cs
@@ -7,10 +7,15 @@ return;
 #pragma warning disable CA2007 // Do not directly await a Task
 
 using Statiq.App;
+using Statiq.Razor;
 
 return await Bootstrapper
     .Factory
     .CreateDefault(args)
+    .BuildPipeline("Render Razor", builder => builder
+        .WithInputReadFiles("**/*.cshtml")
+        .WithProcessModules(new RenderRazor())
+        .WithOutputWriteFiles(".html"))
     .BuildPipeline("Copy Assets", builder => builder
         .WithInputReadFiles("**/*.css")
         .WithOutputWriteFiles())
