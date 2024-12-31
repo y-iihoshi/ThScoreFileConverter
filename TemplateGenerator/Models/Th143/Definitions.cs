434e7fa7cc751e05cc1a4cc4fc918e690cf7beb2
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-01-01 02:23:10 +0900

    Add DisplayAttribute to th14.3 enums

--- TemplateGenerator/Models/Th143/Definitions.cs
+++ TemplateGenerator/Models/Th143/Definitions.cs
@@ -11,51 +11,38 @@ namespace TemplateGenerator.Models.Th143;
 
 public static class Definitions
 {
-    private static readonly IEnumerable<(ItemWithTotal, string)> ItemWithTotalNamesImpl =
-    [
-        (ItemWithTotal.Fablic,   "布"),
-        (ItemWithTotal.Camera,   "カメラ"),
-        (ItemWithTotal.Umbrella, "傘"),
-        (ItemWithTotal.Lantern,  "提灯"),
-        (ItemWithTotal.Orb,      "陰陽玉"),
-        (ItemWithTotal.Bomb,     "ボム"),
-        (ItemWithTotal.Jizou,    "地蔵"),
-        (ItemWithTotal.Doll,     "人形"),
-        (ItemWithTotal.Mallet,   "小槌"),
-        (ItemWithTotal.NoItem,   "未使用"),
-        (ItemWithTotal.Total,    "合計"),
-    ];
-
     public static string Title { get; } = StringResources.TH143;
 
     public static IReadOnlyDictionary<string, (string Id, string Name)> DayNames { get; } = new[]
     {
-        (Day.First,   ("Day1",    "一日目")),
-        (Day.Second,  ("Day2",    "二日目")),
-        (Day.Third,   ("Day3",    "三日目")),
-        (Day.Fourth,  ("Day4",    "四日目")),
-        (Day.Fifth,   ("Day5",    "五日目")),
-        (Day.Sixth,   ("Day6",    "六日目")),
-        (Day.Seventh, ("Day7",    "七日目")),
-        (Day.Eighth,  ("Day8",    "八日目")),
-        (Day.Ninth,   ("Day9",    "九日目")),
-        (Day.Last,    ("LastDay", "最終日")),
-    }.ToPatternKeyedDictionary();
+        (Day.First,   "Day1"),
+        (Day.Second,  "Day2"),
+        (Day.Third,   "Day3"),
+        (Day.Fourth,  "Day4"),
+        (Day.Fifth,   "Day5"),
+        (Day.Sixth,   "Day6"),
+        (Day.Seventh, "Day7"),
+        (Day.Eighth,  "Day8"),
+        (Day.Ninth,   "Day9"),
+        (Day.Last,    "LastDay"),
+    }.ToDictionary(
+        static pair => pair.Item1.ToPattern(),
+        static pair => (pair.Item2, pair.Item1.ToDisplayName()));
 
     public static IReadOnlyDictionary<string, int> NumScenesPerDay { get; } =
         EnumHelper<Day>.Enumerable.ToDictionary(
-            static day => day.ToPattern(),
+            EnumExtensions.ToPattern,
             static day => SpellCards.Count(pair => pair.Key.Day == day));
 
     public static IReadOnlyDictionary<string, (string ShortName, string LongName)> ItemWithTotalNames { get; } =
-        ItemWithTotalNamesImpl.ToDictionary(
-            static pair => pair.Item1.ToPattern(),
-            static pair => (pair.Item2, pair.Item1.ToDisplayName()));
+        EnumHelper<ItemWithTotal>.Enumerable.ToDictionary(
+            EnumExtensions.ToPattern,
+            static item => (item.ToDisplayShortName(), item.ToDisplayName()));
 
     public static IReadOnlyDictionary<string, (string ShortName, string LongName)> ItemNames { get; } =
-        ItemWithTotalNamesImpl.Where(static pair => pair.Item1 != ItemWithTotal.Total).ToDictionary(
-            static pair => pair.Item1.ToPattern(),
-            static pair => (pair.Item2, pair.Item1.ToDisplayName()));
+        EnumHelper<ItemWithTotal>.Enumerable.Where(static item => item != ItemWithTotal.Total).ToDictionary(
+            EnumExtensions.ToPattern,
+            static item => (item.ToDisplayShortName(), item.ToDisplayName()));
 
     public static IEnumerable<string> ItemKeysTotalFirst { get; } = ItemWithTotalNames.Keys.RotateRight();
 
--- ThScoreFileConverter.Core/Models/Th143/ItemWithTotal.cs
+++ ThScoreFileConverter.Core/Models/Th143/ItemWithTotal.cs
@@ -17,70 +17,70 @@ public enum ItemWithTotal
     /// <summary>
     /// Nimble Fabric.
     /// </summary>
-    [Display(Name = "ひらり布")]
+    [Display(Name = "ひらり布", ShortName = "布")]
     [Pattern("1")]
     Fablic,
 
     /// <summary>
     /// Tengu's Toy Camera.
     /// </summary>
-    [Display(Name = "天狗のトイカメラ")]
+    [Display(Name = "天狗のトイカメラ", ShortName = "カメラ")]
     [Pattern("2")]
     Camera,
 
     /// <summary>
     /// Gap Folding Umbrella.
     /// </summary>
-    [Display(Name = "隙間の折りたたみ傘")]
+    [Display(Name = "隙間の折りたたみ傘", ShortName = "傘")]
     [Pattern("3")]
     Umbrella,
 
     /// <summary>
     /// Ghastly Send-Off Lantern.
     /// </summary>
-    [Display(Name = "亡霊の送り提灯")]
+    [Display(Name = "亡霊の送り提灯", ShortName = "提灯")]
     [Pattern("4")]
     Lantern,
 
     /// <summary>
     /// Bloodthirsty Yin-Yang Orb.
     /// </summary>
-    [Display(Name = "血に飢えた陰陽玉")]
+    [Display(Name = "血に飢えた陰陽玉", ShortName = "陰陽玉")]
     [Pattern("5")]
     Orb,
 
     /// <summary>
     /// Four-Foot Magic Bomb.
     /// </summary>
-    [Display(Name = "四尺マジックボム")]
+    [Display(Name = "四尺マジックボム", ShortName = "ボム")]
     [Pattern("6")]
     Bomb,
 
     /// <summary>
     /// Substitute Jizou.
     /// </summary>
-    [Display(Name = "身代わり地蔵")]
+    [Display(Name = "身代わり地蔵", ShortName = "地蔵")]
     [Pattern("7")]
     Jizou,
 
     /// <summary>
     /// Cursed Decoy Doll.
     /// </summary>
-    [Display(Name = "呪いのデコイ人形")]
+    [Display(Name = "呪いのデコイ人形", ShortName = "人形")]
     [Pattern("8")]
     Doll,
 
     /// <summary>
     /// A Miracle Mallet Replica.
     /// </summary>
-    [Display(Name = "打ち出の小槌（レプリカ）")]
+    [Display(Name = "打ち出の小槌（レプリカ）", ShortName = "小槌")]
     [Pattern("9")]
     Mallet,
 
     /// <summary>
     /// No item.
     /// </summary>
-    [Display(Name = "ノーアイテム")]
+    [Display(Name = "ノーアイテム", ShortName = "未使用")]
     [Pattern("0")]
     NoItem,
 
