dc33fd44b52af0f12c7ccbcebe567a689952dfac
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-01-06 00:39:05 +0900

    Fix attributes of th15.5 chara-related enums

--- ThScoreFileConverter.Core/Models/Th155/StoryCharaWithTotal.cs
+++ ThScoreFileConverter.Core/Models/Th155/StoryCharaWithTotal.cs
@@ -16,66 +16,88 @@ public enum StoryCharaWithTotal
     /// Hakurei Reimu and Ibaraki Kasen.
     /// </summary>
     [Pattern("RK")]
+    [Character("Reimu")]
+    [Character("Kasen", 1)]
     ReimuKasen,
 
     /// <summary>
     /// Kirisame Marisa and Komeiji Koishi.
     /// </summary>
     [Pattern("MK")]
+    [Character("Marisa")]
+    [Character("Koishi", 1)]
     MarisaKoishi,
 
     /// <summary>
     /// Kawashiro Nitori and Hata no Kokoro.
     /// </summary>
     [Pattern("NK")]
+    [Character("Nitori")]
+    [Character("Kokoro", 1)]
     NitoriKokoro,
 
     /// <summary>
     /// Futatsuiwa Mamizou and Fujiwara no Mokou.
     /// </summary>
     [Pattern("MM")]
+    [Character("Mamizou")]
+    [Character("Mokou", 1)]
     MamizouMokou,
 
     /// <summary>
     /// Toyosatomimi no Miko and Hijiri Byakuren.
     /// </summary>
     [Pattern("MB")]
+    [Character("Miko")]
+    [Character("Byakuren", 1)]
     MikoByakuren,
 
     /// <summary>
     /// Mononobe no Futo and Kumoi Ichirin.
     /// </summary>
     [Pattern("FI")]
+    [Character("Futo")]
+    [Character("Ichirin", 1)]
     FutoIchirin,
 
     /// <summary>
     /// Reisen Udongein Inaba and Doremy Sweet.
     /// </summary>
     [Pattern("RD")]
+    [Character("Reisen")]
+    [Character("Doremy", 1)]
     ReisenDoremy,
 
     /// <summary>
     /// Usami Sumireko and Doremy Sweet.
     /// </summary>
     [Pattern("SD")]
+    [Character("Sumireko")]
+    [Character("Doremy", 1)]
     SumirekoDoremy,
 
     /// <summary>
     /// Hinanawi Tenshi and Sukuna Shinmyoumaru.
     /// </summary>
     [Pattern("TS")]
+    [Character("Tenshi")]
+    [Character("Shinmyoumaru", 1)]
     TenshiShinmyoumaru,
 
     /// <summary>
     /// Yakumo Yukari and Hakurei Reimu.
     /// </summary>
     [Pattern("YR")]
+    [Character("Yukari")]
+    [Character("Reimu", 1)]
     YukariReimu,
 
     /// <summary>
     /// Yorigami Joon and Yorigami Shion.
     /// </summary>
     [Pattern("JS")]
+    [Character("Joon")]
+    [Character("Shion", 1)]
     JoonShion,
 
     /// <summary>
