3201e9662a7e4b00b9e3410a819b2dbb3bebc0a4
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-01-05 22:02:23 +0900

    Fix attributes of th13 chara-related enums

--- ThScoreFileConverter.Core/Models/Th13/Chara.cs
+++ ThScoreFileConverter.Core/Models/Th13/Chara.cs
@@ -15,24 +15,28 @@ public enum Chara
     /// <summary>
     /// Hakurei Reimu.
     /// </summary>
-    [EnumAltName("RM")]
+    [Pattern("RM")]
+    [Character(nameof(Reimu))]
     Reimu,
 
     /// <summary>
     /// Kirisame Marisa.
     /// </summary>
-    [EnumAltName("MR")]
+    [Pattern("MR")]
+    [Character(nameof(Marisa))]
     Marisa,
 
     /// <summary>
     /// Kochiya Sanae.
     /// </summary>
-    [EnumAltName("SN")]
+    [Pattern("SN")]
+    [Character(nameof(Sanae))]
     Sanae,
 
     /// <summary>
     /// Konpaku Youmu.
     /// </summary>
-    [EnumAltName("YM")]
+    [Pattern("YM")]
+    [Character(nameof(Youmu))]
     Youmu,
 }
