7d50834d1263ead4f180517820f0fc9a305b549e
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-01-05 18:02:42 +0900

    Fix attributes of th09 chara-related enums

--- TemplateGenerator/Models/Th09/Definitions.cs
+++ TemplateGenerator/Models/Th09/Definitions.cs
@@ -10,25 +10,31 @@ public class Definitions : Models.Definitions
 {
     public static string Title { get; } = StringResources.TH09;
 
-    public static IReadOnlyDictionary<string, (string Id, string ShortName, string LongName)> CharacterNames { get; } = new[]
-    {
-        (Chara.Reimu,     "霊夢",       "博麗 霊夢"),
-        (Chara.Marisa,    "魔理沙",     "霧雨 魔理沙"),
-        (Chara.Sakuya,    "咲夜",       "十六夜 咲夜"),
-        (Chara.Youmu,     "妖夢",       "魂魄 妖夢"),
-        (Chara.Reisen,    "鈴仙",       "鈴仙・優曇華院・イナバ"),
-        (Chara.Cirno,     "チルノ",     "チルノ"),
-        (Chara.Lyrica,    "リリカ",     "リリカ・プリズムリバー"),
-        (Chara.Mystia,    "ミスティア", "ミスティア・ローレライ"),
-        (Chara.Tewi,      "てゐ",       "因幡 てゐ"),
-        (Chara.Aya,       "文",         "射命丸 文"),
-        (Chara.Medicine,  "メディスン", "メディスン・メランコリー"),
-        (Chara.Yuuka,     "幽香",       "風見 幽香"),
-        (Chara.Komachi,   "小町",       "小野塚 小町"),
-        (Chara.Eiki,      "映姫",       "四季映姫・ヤマザナドゥ"),
-    }.ToDictionary(
-        static tuple => tuple.Item1.ToShortName(),
-        static tuple => (tuple.Item1.ToString(), tuple.Item2, tuple.Item3));
+    public static IReadOnlyDictionary<string, (string Id, string ShortName, string LongName)> CharacterNames { get; } =
+#if false
+        EnumHelper<Chara>.Enumerable.Where(static chara => chara is not Chara.Lunasa and not Chara.Merlin)
+#else
+        new[]
+        {
+            Chara.Reimu,
+            Chara.Marisa,
+            Chara.Sakuya,
+            Chara.Youmu,
+            Chara.Reisen,
+            Chara.Cirno,
+            Chara.Lyrica,
+            Chara.Mystia,
+            Chara.Tewi,
+            Chara.Aya,
+            Chara.Medicine,
+            Chara.Yuuka,     // NOTE: The sort order does not match with the defined values in the Chara enum.
+            Chara.Komachi,
+            Chara.Eiki,
+        }
+#endif
+        .ToDictionary(
+            static chara => chara.ToPattern(),
+            static chara => (chara.ToString(), chara.ToCharaName(), chara.ToCharaFullName()));
 
     public static IReadOnlyList<string> RankOrdinals { get; } =
     [
