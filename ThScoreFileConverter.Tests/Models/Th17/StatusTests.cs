5b190812393da28c35066b61b8794cfb6a97e9de
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-02-02 15:52:40 +0900

    Rewrite validations of Tests.Models.Th17

--- ThScoreFileConverter.Tests/Models/Th17/StatusTests.cs
+++ ThScoreFileConverter.Tests/Models/Th17/StatusTests.cs
@@ -4,6 +4,21 @@ using Chapter = ThScoreFileConverter.Models.Th10.Chapter;
 
 namespace ThScoreFileConverter.Tests.Models.Th17;
 
+internal static class StatusExtensions
+{
+    internal static void ShouldBe(this IStatus actual, IStatus expected)
+    {
+        actual.Signature.ShouldBe(expected.Signature);
+        actual.Checksum.ShouldBe(expected.Checksum);
+        actual.Version.ShouldBe(expected.Version);
+        actual.Size.ShouldBe(expected.Size);
+        actual.LastName.ShouldBe(expected.LastName);
+        actual.BgmFlags.ShouldBe(expected.BgmFlags);
+        actual.TotalPlayTime.ShouldBe(expected.TotalPlayTime);
+        actual.Achievements.ShouldBe(expected.Achievements);
+    }
+}
+
 [TestClass]
 public class StatusTests
 {
@@ -38,18 +53,6 @@ public class StatusTests
             TestUtils.MakeRandomArray(0x438));
     }
 
-    internal static void Validate(IStatus expected, IStatus actual)
-    {
-        actual.Signature.ShouldBe(expected.Signature);
-        actual.Checksum.ShouldBe(expected.Checksum);
-        actual.Version.ShouldBe(expected.Version);
-        actual.Size.ShouldBe(expected.Size);
-        actual.LastName.ShouldBe(expected.LastName);
-        actual.BgmFlags.ShouldBe(expected.BgmFlags);
-        actual.TotalPlayTime.ShouldBe(expected.TotalPlayTime);
-        actual.Achievements.ShouldBe(expected.Achievements);
-    }
-
     [TestMethod]
     public void StatusTest()
     {
@@ -58,7 +61,7 @@ public class StatusTests
         var chapter = TestUtils.Create<Chapter>(MakeByteArray(mock));
         var status = new Status(chapter);
 
-        Validate(mock, status);
+        status.ShouldBe(mock);
         status.IsValid.ShouldBeFalse();
     }
 
