40ff66dc9328abaaba682ff520bbb27ff5586088
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2025-05-25 18:46:48 +0900

    Add benchmark tests about array copy

--- ThScoreFileConverter.Benchmarks/Program.cs
+++ ThScoreFileConverter.Benchmarks/Program.cs
@@ -3,4 +3,8 @@ using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
 
 var config = DefaultConfig.Instance.AddDiagnoser(MemoryDiagnoser.Default);
+#if false
 _ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAllJoined(config, args);
+#else
+_ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+#endif
---
+++ ThScoreFileConverter.Benchmarks/Scenarios/ArrayCopy.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ThScoreFileConverter.Benchmarks.Scenarios;
+
+public class ArrayCopy
+{
+    private readonly int[] source = [.. Enumerable.Range(0, 1000)];
+
+    [Benchmark]
+    public int[] CopyTo()
+    {
+        var destination = new int[this.source.Length];
+        this.source.CopyTo(destination, 0);
+        return destination;
+    }
+
+    [Benchmark]
+    public int[] CollectionExpression()
+    {
+        int[] destination = [.. this.source];
+        return destination;
+    }
+}
